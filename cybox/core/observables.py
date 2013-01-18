7296fcaf752ab8fb6c5a8f858b51da33e0285ba0
Ivan Kirillov <ikirillov@mitre.org>
2013-01-18 14:09:53 -0500

    Added classes and made methods corresponding classmethods for core modules

--- cybox/core/observables.py
+++ cybox/core/observables.py
@@ -1,13 +1,19 @@
 import cybox.bindings.cybox_core_1_0 as core_binding
-import cybox.core.observable as observable
+from cybox.core.observable import observable
 
-def create_from_dict(observables_dict):
-    """Create the Observables Python object representation from an input dictionary"""
-    pass
+class observables(object):
+    def __init__(self):
+        pass
 
-def parse_into_dict(observables):
-    """Parse the observables into a dictionary-esque representation"""
-    observables_list = []
-    for observable in observables.get_Observable():
-        observables_list.append(observable.parse_into_dict(observable))
-    return observables_list
+    @classmethod
+    def create_from_dict(cls, observables_dict):
+        """Create the Observables Python object representation from an input dictionary"""
+        pass
+
+    @classmethod
+    def parse_into_dict(cls, observables):
+        """Parse the observables into a dictionary-esque representation"""
+        observables_list = []
+        for observable in observables.get_Observable():
+            observables_list.append(observable.parse_into_dict(observable))
+        return observables_list
