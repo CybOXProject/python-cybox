c4e65b0c919956b324fecc31e1d02355bc5ea3a7
Bryan Worrell <bworrell@mitre.org>
2013-01-22 09:44:57 -0500

    added an api method to create an email address observable

--- cybox/api.py
+++ cybox/api.py
@@ -4,10 +4,6 @@ CybOX Common Indicator API
 
 An api for creating observables for common indicators:
 ipv4 addresses, domain names, file hashes, and urls.
-
-This module depends on the cybox_helper code found at
-the MAEC GitHub project: 
-https://github.com/MAECProject/Tools/tree/development/API/cybox_helper
 '''
 
 import sys
@@ -19,7 +15,6 @@ import cybox.bindings.file_object_1_3 as file_object # binding
 
 from cybox.objects.uri_object import uri_object as uri_helper
 from cybox.objects.address_object import address_object as address_helper
-
 
 def create_cybox_id(item_type = "guid"):
     '''Returns a unique id to be used by a CybOX entity'''
@@ -51,7 +46,7 @@ def create_observable(cybox_object):
 def create_ipv4_observable(ipv4_address):
     '''Creates a CybOX Observable representing an IPv4 address'''
    
-    ipv4_object = address_helper.create_from_dict({'address_value':ipv4_address})
+    ipv4_object = address_helper.create_from_dict({'address_value':ipv4_address, 'category':'ipv4-addr'})
     observable = create_observable(ipv4_object)
     
     return observable
@@ -72,6 +67,17 @@ def create_ipv4_list_observables(list_ipv4_addresses):
     return list_observables 
 
 
+
+def create_email_address_observable(email_address):
+    '''Creates a CybOX Observable representing an IPv4 address'''
+   
+    email_address_object = address_helper.create_from_dict({'address_value':email_address, 'category':'e-mail'})
+    observable = create_observable(email_address_object)
+    
+    return observable
+
+
+
 def create_domain_name_observable(domain_name):
     '''Creates a CybOX Observable representing a domain name.'''
      
@@ -87,15 +93,16 @@ def create_file_hash_observable(fn, hash_value, hash_type):
     Note: The cybox_helper module for file_object is under
     heavy development at this time and will not be used.  
     '''
-    hash_name_type      = cybox_common.HashNameType(valueOf_= hash_type)
-    hash_value_type     = cybox_common.SimpleHashValueType(valueOf_ = hash_value)
-    hash_object         = cybox_common.HashType(Type=hash_name_type, Simple_Hash_Value=hash_value_type)
-    hash_list_object    = cybox_common.HashListType(Hash=[hash_object]) 
+    hash_name_type  = cybox_common.HashNameType(valueOf_= hash_type)
+    hash_value_type = cybox_common.SimpleHashValueType(valueOf_ = hash_value)
+    hash_object     = cybox_common.HashType(Type=hash_name_type, Simple_Hash_Value=hash_value_type)
+    hash_list       = cybox_common.HashListType(Hash=[hash_object]) 
     
-    file_object_        = file_object.FileObjectType(
+    file_object_     = file_object.FileObjectType(
                                                         File_Name   = cybox_common.StringObjectAttributeType(valueOf_=fn),
-                                                        Hashes      = hash_list_object
+                                                        Hashes      = hash_list
                                                     )
+    
     file_object_.set_anyAttributes_({'xsi:type' : 'FileObj:FileObjectType'})
     observable      = create_observable(file_object_)
     
