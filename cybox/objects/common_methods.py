99b1bb029f316b61f28a70bb5c7824592f524c22
Ivan Kirillov <ikirillov@mitre.org>
2013-01-28 09:38:49 -0500

    Updated test_value method to work with input dictionaries

--- cybox/objects/common_methods.py
+++ cybox/objects/common_methods.py
@@ -1,8 +1,11 @@
 #Common utility methods
 
-#Test if a value is not None and has a length greater than 0
+#Test if a dictionary value is not None and has a length greater than 0
 def test_value(value):
-    if value is not None and len(str(value)) > 0:
-        return True
+    if value.get('value') is not None:
+        if value.get('value')  is not None and len(str(value.get('value'))) > 0:
+            return True 
+        else:
+            return False
     else:
         return False
