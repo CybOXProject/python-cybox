c7ec2805d1c3dde9ff3bf8caacf0bac474a1d468
Greg Back <gback@mitre.org>
2013-02-01 16:13:57 -0500

    Clean up and document 'test_value' function.

--- cybox/utils.py
+++ cybox/utils.py
@@ -1,11 +1,13 @@
-#Common utility methods
+"""Common utility methods"""
 
-#Test if a dictionary value is not None and has a length greater than 0
 def test_value(value):
-    if value.get('value') is not None:
-        if value.get('value')  is not None and len(str(value.get('value'))) > 0:
-            return True 
-        else:
-            return False
-    else:
-        return False
+    """
+    Test if a dictionary contains a "value" key whose value is not None
+    and has a length greater than 0.
+
+    We explicitly want to return True even if the value is False or 0, since
+    some parts of the standards are boolean or allow a 0 value, and we want to
+    distinguish the case where the "value" key is omitted entirely.
+    """
+    v = value.get('value', None)
+    return (v is not None) and (len(str(v)) > 0)
