7c25f1b1749f5272ec94638617ef27f43ebba756
Ivan Kirillov <ikirillov@mitre.org>
2013-02-04 14:50:51 -0500

    Modified utils to support non-dictionary inputs

--- cybox/utils.py
+++ cybox/utils.py
@@ -2,12 +2,22 @@
 
 def test_value(value):
     """
-    Test if a dictionary contains a "value" key whose value is not None
-    and has a length greater than 0.
+    Test if an input string value is not None and has a length grater than 0 or 
+    if a dictionary contains a "value" key whose value is not None and has a length 
+    greater than 0.
 
     We explicitly want to return True even if the value is False or 0, since
     some parts of the standards are boolean or allow a 0 value, and we want to
     distinguish the case where the "value" key is omitted entirely.
     """
-    v = value.get('value', None)
+    if isinstance(value, dict):
+        v = value.get('value', None)
+    elif isinstance(value, str):
+        v = value
+    elif isinstance(value, int):
+        v = value
+    elif isinstance(value, float):
+        v = value
+    else: 
+        v = None
     return (v is not None) and (len(str(v)) > 0)
