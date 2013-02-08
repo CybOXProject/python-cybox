b4cf1032ab102b6e7be3c52d03e03a2fad6d698d
Ivan Kirillov <ikirillov@mitre.org>
2013-02-08 13:28:19 -0500

    Updated core files to account for new naming conventions

--- cybox/core/stateful_measure.py
+++ cybox/core/stateful_measure.py
@@ -1,6 +1,6 @@
 import cybox.bindings.cybox_core_1_0 as core_binding
-from cybox.core.structured_text import structured_text
-from cybox.core.object import cybox_object
+from cybox.core.structured_text import Structured_Text
+from cybox.core.object import Object  
 
 class stateful_measure(object):
     def __init__(self):
@@ -12,15 +12,15 @@ class stateful_measure(object):
         pass
 
     @classmethod
-    def dict_from_object(cls, stateful_measure):
+    def dict_from_object(cls, stateful_measure_obj):
         """Parse and return a dictionary for a stateful measure"""
         stateful_measure_dict = {}
         if stateful_measure.get_name() is not None:
-            stateful_measure_dict['name'] = stateful_measure.get_Name()
+            stateful_measure_dict['name'] = stateful_measure_obj.get_Name()
         if stateful_measure.get_has_changed() is not None:
-            stateful_measure_dict['has_changed'] = stateful_measure.get_has_changed()
+            stateful_measure_dict['has_changed'] = stateful_measure_obj.get_has_changed()
         if stateful_measure.get_Description() is not None:
-            stateful_measure_dict['description'] = structured_text.dict_from_object(stateful_measure.get_Description())
+            stateful_measure_dict['description'] = stateful_measure_obj.dict_from_object(stateful_measure.get_Description())
         if stateful_measure.get_Object() is not None:
-            stateful_measure_dict['object'] = cybox_object.dict_from_object(stateful_measure.get_Object())
+            stateful_measure_dict['object'] = Object.dict_from_object(stateful_measure.get_Object())
         return stateful_measure_dict
