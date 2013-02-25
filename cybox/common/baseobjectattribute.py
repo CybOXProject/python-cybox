646a23d7e68599f02f5eb8decbb58076b39e5a74
Ivan Kirillov <ikirillov@mitre.org>
2013-02-25 11:18:26 -0500

    Modified baseobjectattribute to support a non-dict (singular value) input for object_from_dict

--- cybox/common/baseobjectattribute.py
+++ cybox/common/baseobjectattribute.py
@@ -5,28 +5,31 @@ class Base_Object_Attribute(object):
         pass
 
     @classmethod
-    def object_from_dict(cls, element_object, element_dict):
+    def object_from_dict(cls, element_object, element_values):
         """Create the BaseObjectAttributeType object representation from an input dictionary"""
-        for key, value in element_dict.items():
-            if key == 'id': element_object.set_id(value)
-            elif key == 'idref': element_object.set_idref(value)
-            elif key == 'datatype': element_object.set_datatype(value)
-            elif key == 'condition': element_object.set_condition(value)
-            elif key == 'pattern_type': element_object.set_pattern_type(value)
-            elif key == 'regex_syntax': element_object.set_regex_syntax(value)
-            elif key == 'start_range': element_object.set_start_range(value)
-            elif key == 'end_range': element_object.set_end_range(value)
-            elif key == 'value_set': element_object.set_value_set(value)
-            elif key == 'has_changed': element_object.set_has_changed(value)
-            elif key == 'trend': element_object.set_trend(value)
-            elif key == 'appears_random': element_object.set_appears_random(value)
-            elif key == 'is_obfuscated': element_object.set_is_obfuscated(value)
-            elif key == 'obfuscation_algorithm_ref': element_object.set_obfuscation_algorithm_ref(value)
-            elif key == 'is_defanged': element_object.set_is_defanged(value)
-            elif key == 'defanging_algorithm_ref': element_object.set_defanging_algorithm_ref(value)
-            elif key == 'refanging_transform_type': element_object.set_refanging_transform_type(value)
-            elif key == 'refanging_transform': element_object.set_refanging_transform(value)
-            elif key == 'value': element_object.set_valueOf_(value)
+        if isinstance(element_values, dict):
+            for key, value in element_values.items():
+                if key == 'id': element_object.set_id(value)
+                elif key == 'idref': element_object.set_idref(value)
+                elif key == 'datatype': element_object.set_datatype(value)
+                elif key == 'condition': element_object.set_condition(value)
+                elif key == 'pattern_type': element_object.set_pattern_type(value)
+                elif key == 'regex_syntax': element_object.set_regex_syntax(value)
+                elif key == 'start_range': element_object.set_start_range(value)
+                elif key == 'end_range': element_object.set_end_range(value)
+                elif key == 'value_set': element_object.set_value_set(value)
+                elif key == 'has_changed': element_object.set_has_changed(value)
+                elif key == 'trend': element_object.set_trend(value)
+                elif key == 'appears_random': element_object.set_appears_random(value)
+                elif key == 'is_obfuscated': element_object.set_is_obfuscated(value)
+                elif key == 'obfuscation_algorithm_ref': element_object.set_obfuscation_algorithm_ref(value)
+                elif key == 'is_defanged': element_object.set_is_defanged(value)
+                elif key == 'defanging_algorithm_ref': element_object.set_defanging_algorithm_ref(value)
+                elif key == 'refanging_transform_type': element_object.set_refanging_transform_type(value)
+                elif key == 'refanging_transform': element_object.set_refanging_transform(value)
+                elif key == 'value': element_object.set_valueOf_(value)
+        else:
+            element_object.set_valueOf_(element_values)
         return element_object
 
     @classmethod
