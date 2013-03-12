b5d367c172a8bdb3a9c165041a63ffe569c2c28b
Greg Back <gback@mitre.org>
2013-03-12 17:03:44 -0400

    Add script to convert CybOX XML to CybOX JSON.

---
+++ examples/xml_to_json.py
@@ -0,0 +1,21 @@
+#!\usr\bin\env python
+
+import os
+import sys
+
+import cybox.bindings.cybox_core_1_0 as core_binding
+from cybox.core import Observables
+
+def from_file(filename):
+    cybox_obj = core_binding.parse(os.path.abspath(filename))
+    return Observables.from_obj(cybox_obj)
+
+def main():
+    if len(sys.argv) < 2:
+        print "Argument required"
+        return
+
+    print from_file(sys.argv[1]).to_json()
+
+if __name__ == "__main__":
+    main()
