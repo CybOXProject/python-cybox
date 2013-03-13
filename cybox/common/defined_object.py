2cb5e84dcc5b10078540b6c1c998b770d2c36a51
Ivan Kirillov <ikirillov@mitre.org>
2013-03-13 09:59:36 -0400

    Added none value checks to from_dict and from_obj methods

--- cybox/common/defined_object.py
+++ cybox/common/defined_object.py
@@ -15,6 +15,8 @@ class DefinedObject(cybox.Entity):
 
     @staticmethod
     def from_obj(defobj_obj):
+        if not defobj_obj:
+            return None
         any_attributes = defined_object.get_anyAttributes_()
         xsi_type = any_attributes.get('{http://www.w3.org/2001/XMLSchema-instance}type')
         if not xsi_type:
@@ -28,9 +30,13 @@ class DefinedObject(cybox.Entity):
 
     @staticmethod
     def from_dict(defobj_dict):
+        if not defobj_dict:
+            return None
+        print defobj_dict
         obj_type = defobj_dict.get('object-type')
-        if not xsi_type:
-            raise ValueError("Dictionary has no xsi-type")
+        print obj_type
+        if not obj_type:
+            raise ValueError("Dictionary has no obj-type")
         klass = cybox.utils.get_class_for_object_type(obj_type)
 
         return klass.from_dict(defobj_dict)
