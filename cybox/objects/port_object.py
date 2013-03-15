a7d1fc9e990414e19b3cb8c05e9adf0a44ba9a4d
Ivan Kirillov <ikirillov@mitre.org>
2013-03-15 08:57:00 -0400

    Added simple API representation of port object

--- cybox/objects/port_object.py
+++ cybox/objects/port_object.py
@@ -1,32 +1,53 @@
 import cybox.utils as utils
-import cybox.bindings.cybox_common_types_1_0 as common_types_binding
+#import cybox.bindings.cybox_common_types_1_0 as common_types_binding
 import cybox.bindings.port_object_1_3 as port_binding
-from cybox.common.baseobjectattribute import Base_Object_Attribute
+from cybox.common import DefinedObject, String, PositiveInteger
 
-class Port(object):
+class Port(DefinedObject):
+    _XSI_TYPE = "PortObjectType"
+
     def __init__(self):
-        pass
-    
-    @classmethod
-    def object_from_dict(cls, port_dict):
-        """Create the Port Object object representation from an input dictionary"""
+        self.port_value = None
+        self.layer4_protocol = None
+
+    def to_obj(self):
         port_obj = port_binding.PortObjectType()
         port_obj.set_anyAttributes_({'xsi:type' : 'PortObj:PortObjectType'})
-        for key, value in port_dict.items():
-            if key == 'port_value' and utils.test_value(value):
-                port_obj.set_Port_Value(Base_Object_Attribute.object_from_dict(common_types_binding.PositiveIntegerObjectAttributeType(datatype='PositiveInteger'),value))
-            elif key == 'layer4_protocol' and utils.test_value(value):
-                port_obj.set_Layer4_Protocol(Base_Object_Attribute.object_from_dict(common_types_binding.StringObjectAttributeType(datatype='String'),value))
+
+        if self.port_value is not None: port_obj.set_Port_Value(self.port_value.to_obj())
+        if self.layer4_protocol is not None: port_obj.set_Layer4_Protocol(self.layer4_protocol.to_obj())
+
         return port_obj
 
-    @classmethod
-    def dict_from_object(cls, port_obj):
-        """Parse and return a dictionary for an Port Object object"""
+    def to_dict(self):
         port_dict = {}
-        if port_obj.get_Port_Value() is not None:
-            port_dict['port_value'] = Base_Object_Attribute.dict_from_object(port_obj.get_Port_Value())
-        if port_obj.get_Layer4_Protocol() is not None:
-            port_dict['layer4_protocol'] = Base_Object_Attribute.dict_from_object(port_obj.get_Layer4_Protocol())
+
+        if self.port_value is not None: port_dict['port_value'] = self.port_value.to_dict()
+        if self.layer4_protocol is not None: port_doct['layer4_protocol'] = self.layer4_protocol.to_dict()
+        port_dict['xsi_type'] = _XSI_TYPE
+
         return port_dict
+    
+    @staticmethod
+    def from_dict(port_dict):
+        if not port_dict:
+            return None
+
+        port_ = Port()
+        port_.port_value = PositiveInteger.from_dict(port_dict.get('port_value'))
+        port_.layer4_protocol = String.from_dict(port_dict.get('layer4_protocol'))
+
+        return port_
+
+    @staticmethod
+    def from_obj(port_obj):
+        if not port_obj:
+            return None
+
+        port_ = Port()
+        port_.port_value = PositiveInteger.from_obj(port_obj.get_Port_Value())
+        port_.layer4_protocol = String.from_obj(port_obj.get_Layer4_Protocol())
+
+        return port_
 
 
