1b69a4c1c185fcf9ad364530d7c5a9e4d0f684b0
Ivan Kirillov <ikirillov@mitre.org>
2013-04-10 16:18:18 -0400

    Added Simple API interface for action

--- cybox/core/action.py
+++ cybox/core/action.py
@@ -1,93 +1,162 @@
+import cybox
 import cybox.utils as utils
-import cybox.bindings.cybox_common_types_1_0 as common_binding
 import cybox.bindings.cybox_core_1_0 as core_binding
-from cybox.common.baseobjectattribute import Base_Object_Attribute
-from cybox.common.measuresource import Measure_Source
+#import cybox.core.structured_text as Structured_Text
+#from cybox.common.measuresource import Measure_Source
+#from cybox.common.baseobjectattribute import Base_Object_Attribute
+#from cybox.common.measuresource import Measure_Source
+from cybox.core.associated_object import AssociatedObject
 
-class Action(object):
+class Action(cybox.Entity):
     def __init__(self):
-        pass
-
-    @classmethod
-    def object_from_dict(cls, action_dict, action_obj = None):
-        """Create the Action object representation from an input dictionary"""
+        self.id = None
+        self.idref = None
+        self.type = None
+        self.name = None
+        self.undefined_name = None
+        self.ordinal_position = None
+        self.action_status = None
+        self.context = None
+        self.network_protocol = None
+        self.timestamp = None
+        self.description = None
+        self.action_aliases = []
+        self.action_arguments = []
+        self.discovery_method = None
+        self.associated_objects = []
+        self.relationships = []
+        self.frequency = None
+        
+    def to_obj(self, action_obj = None):
         if action_obj == None:
             action_obj = core_binding.ActionType()
-        for key, value in action_dict.items():
-            if key == 'id' and utils.test_value(value): action_obj.set_id(value)
-            elif key == 'idref' and utils.test_value(value): action_obj.set_idref(value)
-            elif key == 'type' and utils.test_value(value): action_obj.set_type(value)
-            elif key == 'name' and utils.test_value(value): action_obj.set_name(value)
-            elif key == 'undefined_name' and utils.test_value(value): action_obj.set_undefined_name(value)
-            elif key == 'ordinal_position' and utils.test_value(value): action_obj.set_ordinal_position(value)
-            elif key == 'action_status' and utils.test_value(value): action_obj.set_action_status(value)
-            elif key == 'context' and utils.test_value(value) : action_obj.set_context(value)
-            elif key == 'network_protocol' and utils.test_value(value) : action_obj.set_network_protocol(value)
-            elif key == 'timestamp' and utils.test_value(value) : action_obj.set_timestamp()
-            elif key == 'description' : pass
-            elif key == 'action_aliases' :
-                action_aliases_obj = core_binding.ActionAliasesType()
-                for action_alias in value:
-                    if utils.test_value(action_alias) : action_aliases_obj.add_Action_Alias(action_alias)
-                if action_aliases_obj.hasContent_() : action_obj.set_Action_Aliases(action_aliases_obj)
-            elif key == 'action_arguments':
-                action_arguments_obj = core_binding.ActionArgumentsType()
-                for action_argument_dict in value:
-                    action_argument_obj = cls.__action_argument_obj_from_dict(action_argument_dict)
-                    if action_argument_obj.hasContent_() : action_arguments_obj.add_Action_Argument(action_argument_obj)
-                if action_arguments_obj.hasContent_() : action_obj.set_Action_Arguments(action_arguments_obj)
-            elif key == 'discovery_method':
-                measure_source_obj = Measure_Source.object_from_dict(value)
-                if measure_source_obj.hasContent_() : action_obj.set_Discovery_Method(measure_source_obj)
-            elif key == 'associated_objects':
-                associated_objs = core_binding.AssociatedObjectsType()
-                for associated_obj in value:
-                    associated_objs.add_Associated_Object(associated_obj)
-                if associated_objs.hasContent_() : action_obj.set_Associated_Objects(associated_objs)
-            elif key == 'relationships':
-                relationships_obj = core_binding.RelationshipsType()
-                for relationship_dict in value:
-                    relationships_obj.add_Relationship(cls.__action_relationship_obj_from_dict(relationship_dict))
-            elif key == 'frequency': pass
+        if self.id is not None: action_obj.set_id(self.id)
+        if self.idref is not None: action_obj.set_idref(self.idref)
+        if self.type is not None: action_obj.set_type(self.type)
+        if self.name is not None: action_obj.set_name(self.name)
+        if self.undefined_name is not None: action_obj.set_undefined_name(self.undefined_name)
+        if self.ordinal_position is not None: action_obj.set_ordinal_position(self.ordinal_position)
+        if self.action_status is not None: action_obj.set_action_status(self.action_status)
+        if self.context is not None: action_obj.set_context(self.context)
+        if self.network_protocol is not None: action_obj.set_network_protocol(self.network_protocol)
+        if self.timestamp is not None: action_obj.set_timestamp(self.timestamp)
+        if self.description is not None: action_obj.set_description(self.description.to_obj())
+        if self.action_aliases is not None:
+            action_aliases_obj = core_binding.ActionAliasesType()
+            for action_alias in self.action_aliases:
+                action_aliases_obj.add_Action_Alias(action_alias)
+            action_obj.set_Action_Aliases(action_aliases_obj)
+        if self.action_arguments is not None:
+            action_arguments_obj = core_binding.ActionArgumentsType()
+            for action_argument in self.action_arguments:
+                action_arguments_obj.add_Action_Argument(action_argument.to_obj())
+            action_obj.set_Action_Arguments(action_arguments_obj)
+        if self.discovery_method is not None: action_obj.set_Discovery_Method(self.discovery_method.to_obj())
+        if self.associated_objects is not None: 
+            associated_objects_obj = core_binding.AssociatedObjectsType()
+            for associated_object in self.associated_objects:
+                associated_objects_obj.add_Associated_Object(associated_object.to_obj())
+            action_obj.set_Associated_Objects(associated_objects_obj)
+        if self.relationships is not None:
+            relationships_obj = core_binding.RelationshipsType()
+            for relationship in self.relationships:
+                relationships_obj.add_Relationship(relationship.to_obj())
+            action_obj.set_Relationships(relationships_obj)
+        if self.frequency is not None: action_obj.set_Frequency(self.frequency.to_obj())
+        
         return action_obj
 
-    @classmethod
-    def dict_from_object(cls, action_obj):
-        """Parse and return a dictionary for an Action object"""
+    def to_dict(self):
         action_dict = {}
         return action_dict
-      
-    @classmethod
-    def __action_argument_obj_from_dict(cls, action_argument_dict):
+
+    def from_obj(action_obj):
+        pass
+
+    @staticmethod
+    def from_dict(action_dict, action_cls = None):
+        if not action_dict:
+            return None
+        if action_cls == None:
+            action_cls = Action()
+        action_ = action_cls
+        action_.id = action_dict.get('id')
+        action_.idref = action_dict.get('id')
+        action_.type = action_dict.get('type')
+        action_.name = action_dict.get('name')
+        action_.undefined_name = action_dict.get('undefined_name')
+        action_.ordinal_position = action_dict.get('ordinal_position')
+        action_.action_status = action_dict.get('action_status')
+        action_.context = action_dict.get('context')
+        action_.network_protocol = action_dict.get('network_protocol')
+        action_.timestamp = action_dict.get('timestamp')
+        #action_.description = StructuredText.from_dict(action.get('description'))
+        action_.action_aliases = action_dict.get('action_aliases')
+        action_.action_arguments = [ActionArgument.from_dict(x) for x in action_dict.get('action_arguments')]
+        #action_.discovery_method = MeasureSource.from_dict(action_dict.get('discovery_method'))
+        action_.associated_objects = [AssociatedObject.from_dict(x) for x in action_dict.get('associated_objects')]
+        action_.relationships = [ActionRelationship.from_dict(x) for x in action_dict.get('relationships')]
+        #action_.frequency = Frequency.from_dict(action_dict.get('frequency'))
+        return action_
+
+    @staticmethod
+    def from_obj(action_obj):
+        pass
+
+class ActionArgument(cybox.Entity):
+    def __init__(self):
+        self.defined_argument_name = None
+        self.undefined_argument_name = None
+        self.argument_value = None
+
+    def to_obj(self):
         action_argument_obj = core_binding.ActionArgumentType()
-        for key, value in action_argument_dict.items():
-            if key == 'defined_argument_name' and utils.test_value(value): action_argument_obj.set_defined_argument_name(value)
-            elif key == 'undefined_argument_name' and utils.test_value(value): action_argument_obj.set_undefined_argument_name(value)
-            elif key == 'argument_value' and utils.test_value(value): action_argument_obj.set_argument_value(value)
+        if self.defined_argument_name is not None: action_argument_obj.set_defined_argument_name(self.defined_argument_name)
+        if self.undefined_argument_name is not None: action_argument_obj.set_undefined_argument_name(self.undefined_argument_name)
+        if self.argument_value is not None: action_argument_obj.set_argument_value(self.argument_value)
         return action_argument_obj
-    
-    @classmethod
-    def __action_argument_dict_from_obj(cls, action_argument_obj):
-        action_argument_dict = {}
-        if action_argument_obj.get_defined_argument_name() is not None: action_argument_dict['defined_argument_name'] = action_argument_obj.get_defined_argument_name()
-        if action_argument_obj.get_undefined_argument_name() is not None: action_argument_dict['undefined_argument_name'] = action_argument_obj.get_undefined_argument_name()
-        if action_argument_obj.get_argument_value() is not None: action_argument_dict['argument_value'] = action_argument_obj.get_argument_value()
-        return action_argument_dict
+
+    def to_dict(self):
+        pass
+
+    @staticmethod
+    def from_dict(action_argument_dict):
+        if not action_argument_dict:
+            return None
+        action_argument_ = ActionArgument()
+        action_argument_.defined_argument_name = action_argument_dict.get('defined_argument_name')
+        action_argument_.undefined_argument_name = action_argument_dict.get('undefined_argument_name')
+        action_argument_.argument_value = action_argument_dict.get('argument_value')
+        return action_argument_
 
     @classmethod
-    def __action_relationship_obj_from_dict(cls, action_relationship_dict):
+    def from_obj(action_argument_obj):
+        pass
+
+class ActionRelationship(cybox.Entity):
+    def __init__(self):
+        self.type = None
+        self.action_references = []
+
+    def to_obj(self):
         action_relationship_obj = core_binding.ActionRelationshipType()
-        for key, value in action_relationship_dict.items():
-            if key == 'type' and utils.test_value(value) : action_relationship_obj.set_type(value)
-            if key == 'action_references':
-                for action_reference in action_references:
-                    action_reference_obj = core_binding.ActionReferenceType()
-                    for action_reference_key, action_reference_value in action_reference.items():
-                        if action_reference_key == 'action_id' and utils.test_value(action_reference_value):
-                            action_reference_obj.set_action_id(value)
-                    action_relationship_obj.add_Action_Reference(action_reference_obj)
-        return action_relationship_obj
+        if self.type is not None: action_relationship_obj.set_type_(self.type)
+        if self.action_references is not None: 
+            for action_reference in self.action_references:
+                action_relationship_obj.add_Action_Reference(core_binding.ActionReferenceType(action_id=action_reference))
 
-    @classmethod
-    def __action_relationship_dict_from_obj(cls, action_relationship_obj):
+    def to_dict(self):
+        pass
+
+    @staticmethod
+    def from_dict(action_relationship_dict):
+        if not action_relationship_dict:
+            return None
+        action_relationship_ = ActionRelationship()
+        action_relationship_.type = action_relationship_dict.get('type')
+        action_relationship_.action_references = action_relationship_dict.get('action_references')
+        return action_relationship_
+
+    @staticmethod
+    def from_obj(action_relationship_obj):
         pass
