d86071ea7b359dcdccc226bfdb6fc9f29e7eeba5
Greg Back <gback@mitre.org>
2013-04-12 10:36:08 -0400

    Prepare to release on PyPI
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="cybox",
-    version="1.0b1",
+    version="1.0.0b1",
     author="CybOX Project, MITRE Corporation",
     author_email="cybox@mitre.org",
-    description="An API for parsing and generating CybOX content.",
+    description="A Python library for parsing and generating CybOX content.",
     url="http://cybox.mitre.org",
     packages=find_packages(),
     install_requires=['lxml>=2.3', 'python-dateutil'],
