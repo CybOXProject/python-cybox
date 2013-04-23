00faab605f9899c3a84a351182a89edb8b785fc7
Greg Back <gback@mitre.org>
2013-04-23 11:54:28 -0400

    Clean up and simplify StructuredText

--- cybox/common/__init__.py
+++ cybox/common/__init__.py
@@ -1,10 +1,10 @@
 from properties import *
+from object_properties import ObjectProperties
+from structured_text import StructuredText
+from hash import Hash, HashList
+
 from contributor import Contributor
 from daterange import DateRange
-from object_properties import ObjectProperties
-from hash import Hash, HashList
 from personnel import Personnel
 from time import Time
-from structured_text import StructuredText, Block, StructuredGroup
-
 
