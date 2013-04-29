4dcd95e6259f53a95eb25caeec0f90ff4ca91086
Ivan Kirillov <ikirillov@mitre.org>
2013-04-29 10:29:24 -0400

    Updated for cybox 2.0 and new list entity type

--- cybox/common/extracted_string_list.py
+++ cybox/common/extracted_string_list.py
@@ -1,24 +1,26 @@
+import cybox
 import cybox.bindings.cybox_common as common_types_binding
 from cybox.common.extracted_string import ExtractedString
 
-class ExtractedStringList(object):
-    def __init__(self):
-        self.strings = []
+class ExtractedStringList(cybox.EntityList):
+    _contained_type = ExtractedString
 
-    def add(self, extracted_string):
-        self.strings.append(extracted_string)
+    def __init__(self):
+        super(ExtractedStringList, self).__init__()
+
+    def _fix_value(self, value):
+        # If the user tries to put a string into a list, convert it to an ExtractedString.
+        if isinstance(value, basestring):
+            return ExtractedString(value)
 
     def to_obj(self):
         ex_string_list_obj = common_types_binding.ExtractedStringsType()
-        for string_obj in self.strings:
-            ex_string_list_obj.add_String(string_obj.to_obj())
+        for extracted_string_ in self:
+            ex_string_list_obj.add_String(extracted_string_.to_obj())
         return ex_string_list_obj
 
     def to_list(self):
-        ex_string_list = []
-        for string_obj in self.strings:
-            ex_string_list.append(string_obj.to_dict())
-        return ex_string_list
+        return [ex_string.to_dict() for ex_string in self]
 
     @staticmethod
     def from_list(extracted_string_list):
@@ -27,7 +29,7 @@ class ExtractedStringList(object):
 
         extracted_string_list_ = ExtractedStringList()
         for extracted_string_dict in extracted_string_list:
-            extracted_string_list_.add(ExtractedString.from_dict(extracted_string_dict))
+            extracted_string_list.append(ExtractedString.from_dict(extracted_string_dict))
         return extracted_string_list_
 
     @staticmethod
@@ -37,5 +39,5 @@ class ExtractedStringList(object):
 
         extracted_string_list_ = ExtractedStringList()
         for extracted_string_obj in extracted_string_list_obj.get_String():
-            extracted_string_list_.add(ExtractedString.from_obj(extracted_string_obj))
+            extracted_string_list_.append(ExtractedString.from_obj(extracted_string_obj))
         return extracted_string_list_
