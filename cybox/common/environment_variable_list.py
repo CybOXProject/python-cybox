67047ea9329fc39e9947ed853c7558946c84ac6b
Ivan Kirillov <ikirillov@mitre.org>
2013-05-06 16:31:49 -0400

    Updated environment variable list for proper usage of EntityList

--- cybox/common/environment_variable_list.py
+++ cybox/common/environment_variable_list.py
@@ -4,35 +4,15 @@ from cybox.common.environment_variable import EnvironmentVariable
 
 class EnvironmentVariableList(cybox.EntityList):
     _contained_type = EnvironmentVariable
+    _binding_class = common_binding.EnvironmentVariableListType
 
     def __init__(self):
         super(EnvironmentVariableList, self).__init__()
 
-    def to_obj(self):
-        env_variable_list_obj = common_binding.EnvironmentVariableListType()
-        for environment_variable_ in self:
-            env_variable_list_obj.add_Environment_Variable(environment_variable_.to_obj())
-        return env_variable_list_obj
-
-    def to_list(self):
-        return [env_variable.to_dict() for env_variable in self]
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Environment_Variable(list_)
 
     @staticmethod
-    def from_list(environment_variable_list):
-        if not environment_variable_list:
-            return None
-
-        env_variable_list_ = EnvironmentVariableList()
-        for environment_variable_dict in environment_variable_list:
-            env_variable_list_.append(EnvironmentVariable.from_dict(environment_variable_dict))
-        return env_variable_list_
-
-    @staticmethod
-    def from_obj(environment_variable_list_obj):
-        if not environment_variable_list_obj:
-            return None
-
-        env_variable_list_ = EnvironmentVariableList()
-        for environment_variable_obj in environment_variable_list_obj.get_Environment_Variable():
-            env_variable_list_.append(EnvironmentVariable.from_obj(environment_variable_obj))
-        return env_variable_list_
+    def _get_list(binding_obj):
+        return binding_obj.get_Environment_Variable()
