ed927e474cc1846cba8826b1aeeeba9aca825103
Greg Back <gback@mitre.org>
2013-05-06 09:10:09 -0400

    add some more properties to WhoisEntry object

--- cybox/objects/whois_object.py
+++ cybox/objects/whois_object.py
@@ -1,12 +1,52 @@
 import cybox.bindings.whois_object as whois_binding
 
-from cybox.common import ObjectProperties, String
+import cybox
+from cybox.common import ObjectProperties, BaseProperty, String, DateTime
+from cybox.objects.address_object import Address
 from cybox.objects.uri_object import URI
+
+
+class Nameservers(cybox.EntityList):
+    _binding_class = whois_binding.WhoisNameserversType
+    _contained_type = URI
+
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Nameserver(list_)
+
+    @staticmethod
+    def _get_list(binding_obj):
+        return binding_obj.get_Nameserver()
+
+
+class WhoisStatus(BaseProperty):
+    def __init__(self, *args, **kwargs):
+        BaseProperty.__init__(self, *args, **kwargs)
+        self.datatype = "string"
+
+    def _get_binding_class(self):
+        return whois_binding.WhoisStatusType
+
+
+class WhoisStatuses(cybox.EntityList):
+    _binding_class = whois_binding.WhoisStatusesType
+    _contained_type = WhoisStatus
+
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Status(list_)
+
+    @staticmethod
+    def _get_list(binding_obj):
+        return binding_obj.get_Status()
 
 
 class WhoisEntry(ObjectProperties):
     _XSI_NS = 'WhoisObj'
     _XSI_TYPE = 'WhoisObjectType'
+
+    DNSSEC_SIGNED = "Signed"
+    DNSSEC_UNSIGNED = "Unsigned"
 
     def __init__(self):
         super(WhoisEntry, self).__init__()
@@ -35,6 +75,25 @@ class WhoisEntry(ObjectProperties):
             whois_obj.set_Domain_Name(self.domain_name.to_obj())
         if self.domain_id is not None:
             whois_obj.set_Domain_ID(self.domain_id.to_obj())
+        if self.server_name is not None:
+            whois_obj.set_Server_Name(self.server_name.to_obj())
+        if self.ip_address is not None:
+            whois_obj.set_IP_Address(self.ip_address.to_obj())
+        if self.dnssec is not None:
+            whois_obj.set_DNSSEC(self.dnssec)
+        if self.nameservers:
+            whois_obj.set_Nameservers(self.nameservers.to_obj())
+        if self.status:
+            whois_obj.set_Status(self.status.to_obj())
+        if self.updated_date is not None:
+            whois_obj.set_Updated_Date(self.updated_date.to_obj())
+        if self.creation_date is not None:
+            whois_obj.set_Creation_Date(self.creation_date.to_obj())
+        if self.expiration_date is not None:
+            whois_obj.set_Expiration_Date(self.expiration_date.to_obj())
+
+        if self.sponsoring_registrar is not None:
+            whois_obj.set_Sponsoring_Registrar(self.sponsoring_registrar.to_obj())
 
         return whois_obj
 
@@ -46,6 +105,25 @@ class WhoisEntry(ObjectProperties):
             whois_dict['domain_name'] = self.domain_name.to_dict()
         if self.domain_id is not None:
             whois_dict['domain_id'] = self.domain_id.to_dict()
+        if self.server_name is not None:
+            whois_dict['server_name'] = self.server_name.to_dict()
+        if self.ip_address is not None:
+            whois_dict['ip_address'] = self.ip_address.to_dict()
+        if self.dnssec is not None:
+            whois_dict['dnssec'] = self.dnssec
+        if self.nameservers:
+            whois_dict['nameservers'] = self.nameservers.to_list()
+        if self.status:
+            whois_dict['status'] = self.status.to_list()
+        if self.updated_date is not None:
+            whois_dict['updated_date'] = self.updated_date.to_dict()
+        if self.creation_date is not None:
+            whois_dict['creation_date'] = self.creation_date.to_dict()
+        if self.expiration_date is not None:
+            whois_dict['expiration_date'] = self.expiration_date.to_dict()
+
+        if self.sponsoring_registrar is not None:
+            whois_dict['sponsoring_registrar'] = self.sponsoring_registrar.to_dict()
 
         return whois_dict
 
@@ -59,6 +137,16 @@ class WhoisEntry(ObjectProperties):
 
         whois.domain_name = URI.from_obj(whois_obj.get_Domain_Name())
         whois.domain_id = String.from_obj(whois_obj.get_Domain_ID())
+        whois.server_name = URI.from_obj(whois_obj.get_Server_Name())
+        whois.ip_address = Address.from_obj(whois_obj.get_IP_Address())
+        whois.dnssec = whois_obj.get_DNSSEC()
+        whois.nameservers = Nameservers.from_obj(whois_obj.get_Nameservers())
+        whois.status = WhoisStatuses.from_obj(whois_obj.get_Status())
+        whois.updated_date = DateTime.from_obj(whois_obj.get_Updated_Date())
+        whois.creation_date = DateTime.from_obj(whois_obj.get_Creation_Date())
+        whois.expiration_date = DateTime.from_obj(whois_obj.get_Expiration_Date())
+
+        whois.sponsoring_registrar = String.from_obj(whois_obj.get_Sponsoring_Registrar())
 
         return whois
 
@@ -72,5 +160,15 @@ class WhoisEntry(ObjectProperties):
 
         whois.domain_name = URI.from_dict(whois_dict.get('domain_name'))
         whois.domain_id = String.from_dict(whois_dict.get('domain_id'))
+        whois.server_name = URI.from_dict(whois_dict.get('server_name'))
+        whois.ip_address = Address.from_dict(whois_dict.get('ip_address'), Address.CAT_IPV4)
+        whois.dnssec = whois_dict.get('dnssec')
+        whois.nameservers = Nameservers.from_list(whois_dict.get('nameservers'))
+        whois.status = WhoisStatuses.from_list(whois_dict.get('status'))
+        whois.updated_date = DateTime.from_dict(whois_dict.get('updated_date'))
+        whois.creation_date = DateTime.from_dict(whois_dict.get('creation_date'))
+        whois.expiration_date = DateTime.from_dict(whois_dict.get('expiration_date'))
+
+        whois.sponsoring_registrar = String.from_dict(whois_dict.get('sponsoring_registrar'))
 
         return whois
--- cybox/test/objects/whois_test.py
+++ cybox/test/objects/whois_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from cybox.objects.address_object import Address
 from cybox.objects.uri_object import URI
 from cybox.objects.whois_object import WhoisEntry
 import cybox.test
@@ -11,9 +12,24 @@ class TestWhois(unittest.TestCase, cybox.test.objects.ObjectTestCase):
 
     def test_round_trip(self):
         whois_dict = {
-                        'domain_name': {'value': "www.google.com",
-                                        'type': URI.TYPE_DOMAIN,},
+                        'domain_name': {'value': "www.example.com",
+                                        'type': URI.TYPE_DOMAIN},
                         'domain_id': 'test_id',
+                        'server_name': {'value': "whois.example.com",
+                                        'type': URI.TYPE_DOMAIN},
+                        'ip_address': {'address_value': '1.2.3.4',
+                                       'category': Address.CAT_IPV4},
+                        'dnssec': WhoisEntry.DNSSEC_SIGNED,
+                        'nameservers': [{'value': "ns1.example.com",
+                                         'type': URI.TYPE_DOMAIN},
+                                        {'value': "ns2.example.com",
+                                         'type': URI.TYPE_DOMAIN}],
+                        'status': ['OK', 'PENDING_RESTORE', 'CLIENT_HOLD'],
+                        'updated_date': "2012-05-03T14:49:00-04:00",
+                        'creation_date': "2010-05-03T07:49:00-04:00",
+                        'expiration_date': "2015-05-03T04:49:00-04:00",
+
+                        'sponsoring_registrar': "SRegistrar",
                      }
 
         whois_dict2 = cybox.test.round_trip_dict(WhoisEntry, whois_dict)
