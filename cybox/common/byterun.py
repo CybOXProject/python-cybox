6f77bf63af88aa2c8af23fb05688bc01b561c275
Greg Back <gback@mitre.org>
2013-05-09 14:38:55 -0400

    Improve ByteRun and ByteRuns objects

--- cybox/common/byterun.py
+++ cybox/common/byterun.py
@@ -1,59 +1,98 @@
-import cybox.bindings.cybox_common as common_types_binding
+import cybox
+import cybox.bindings.cybox_common as common_binding
 from cybox.common.baseobjectattribute import Base_Object_Attribute
-from cybox.common import HashList
+from cybox.common import HashList, Integer
 
-class ByteRun(object):
+
+class ByteRun(cybox.Entity):
+
+    offset = cybox.TypedField("Offset", Integer)
+    file_system_offset = cybox.TypedField("File_System_Offset", Integer)
+    image_offset = cybox.TypedField("Image_Offset", Integer)
+    length = cybox.TypedField("Length", Integer)
+    hashes = cybox.TypedField("Hashes", HashList, try_cast=False)
+
     def __init__(self):
+        self.byte_run_data = None
         pass
 
-    @classmethod
-    def object_from_dict(cls, byterun_dict):
-        """Create the ByteRun object representation from an input dictionary"""
-        byterun_obj = common_types_binding.ByteRunType()
-        for key, value in byterun_dict.items():
-            if key == 'offset' :
-                byterun_obj.set_Offset(Base_Object_Attribute.object_from_dict(common_types_binding.IntegerObjectAttributeType(datatype='Integer'),value))
-            elif key == 'file_system_offset' :
-                byterun_obj.set_File_System_Offset(Base_Object_Attribute.object_from_dict(common_types_binding.IntegerObjectAttributeType(datatype='Integer'),value))
-            elif key == 'image_offset' :
-                byterun_obj.set_Image_Offset(Base_Object_Attribute.object_from_dict(common_types_binding.IntegerObjectAttributeType(datatype='Integer'),value))
-            elif key == 'length' :
-                byterun_obj.set_Offset(Base_Object_Attribute.object_from_dict(common_types_binding.IntegerObjectAttributeType(datatype='Integer'),value))
-            elif key == 'hashes' :
-                byterun_obj.set_Hashes(HashList.object_from_dict(value))
-            elif key == 'byte_run_data':
-                byterun_obj.set_Byte_Run_Data(value)
+    def to_obj(self):
+        byterun_obj = common_binding.ByteRunType()
+
+        if self.offset is not None:
+            byterun_obj.set_Offset(self.offset.to_obj())
+        if self.file_system_offset is not None:
+            byterun_obj.set_File_System_Offset(self.file_system_offset.to_obj())
+        if self.image_offset is not None:
+            byterun_obj.set_Image_Offset(self.image_offset.to_obj())
+        if self.length is not None:
+            byterun_obj.set_Length(self.length.to_obj())
+        if self.hashes:
+            byterun_obj.set_Hashes(self.hashes.to_obj())
+        if self.byte_run_data is not None:
+            byterun_obj.set_Byte_Run_Data(self.byte_run_data)
+
         return byterun_obj
 
-    @classmethod
-    def dict_from_object(cls, byterun_obj):
-        """Parse and return a dictionary for a ByteRun object"""
+    def to_dict(self):
         byterun_dict = {}
-        if byterun_obj.get_Offset() is not None: byterun_dict['offset'] = Base_Object_Attribute.dict_from_object(byterun_obj.get_Offset())
-        if byterun_obj.get_File_System_Offset() is not None: byterun_dict['file_system_offset'] = Base_Object_Attribute.dict_from_object(byterun_obj.get_File_System_Offset())  
-        if byterun_obj.get_Image_Offset() is not None: byterun_dict['image_offset'] = Base_Object_Attribute.dict_from_object(byterun_obj.get_Image_Offset())
-        if byterun_obj.get_Length() is not None: byterun_dict['length'] = Base_Object_Attribute.dict_from_object(byterun_obj.get_Length())
-        if byterun_obj.get_Hashes() is not None: byterun_dict['hashes'] = HashList.dict_from_object(byterun_obj.get_Hashes())
-        if byterun_obj.get_Byte_Run_Data() is not None: byterun_dict['byte_run_data'] = {'value' : byterun_obj.get_Byte_Run_Data()}
+
+        if self.offset is not None:
+            byterun_dict['offset'] = self.offset.to_dict()
+        if self.file_system_offset is not None:
+            byterun_dict['file_system_offset'] = self.file_system_offset.to_dict()
+        if self.image_offset is not None:
+            byterun_dict['image_offset'] = self.image_offset.to_dict()
+        if self.length is not None:
+            byterun_dict['length'] = self.length.to_dict()
+        if self.hashes:
+            byterun_dict['hashes'] = self.hashes.to_list()
+        if self.byte_run_data is not None:
+            byterun_dict['byte_run_data'] = self.byte_run_data
+
         return byterun_dict
 
-class ByteRuns(object):
-    def __init__(self):
-        pass
+    @staticmethod
+    def from_obj(byterun_obj):
+        if byterun_obj is None:
+            return None
 
-    @classmethod
-    def object_from_dict(cls, byteruns_list):
-        """Create the ByteRuns object representation from an input list of ByteRun dictionaries"""
-        byteruns_obj = common_types_binding.ByteRunsType()
-        for byterun_dict in byteruns_list():
-            byterun_obj = ByteRun.object_from_dict(byterun_dict)
-            if byterun_obj.hasContent_() : byteruns_obj.add_Byte_Run(byterun_obj)
-        return byteruns_obj
+        byterun = ByteRun()
 
-    @classmethod
-    def dict_from_object(cls, byteruns_obj):
-        """Parse and return a list of ByteRun dictionaries for a ByteRuns object"""
-        byteruns_list = []
-        for byterun_obj in byteruns_obj.get_Byte_Run():
-           byteruns_list.append(ByteRun.dict_from_object(byterun_obj))
-        return byteruns_dict
+        byterun.offset = Integer.from_obj(byterun_obj.get_Offset())
+        byterun.file_system_offset = Integer.from_obj(byterun_obj.get_File_System_Offset())
+        byterun.image_offset = Integer.from_obj(byterun_obj.get_Image_Offset())
+        byterun.length = Integer.from_obj(byterun_obj.get_Length())
+        byterun.hashes = HashList.from_obj(byterun_obj.get_Hashes())
+        byterun.byte_run_data = byterun_obj.get_Byte_Run_Data()
+
+        return byterun
+
+    @staticmethod
+    def from_dict(byterun_dict):
+        if byterun_dict is None:
+            return None
+
+        byterun = ByteRun()
+
+        byterun.offset = Integer.from_dict(byterun_dict.get('offset'))
+        byterun.file_system_offset = Integer.from_dict(byterun_dict.get('file_system_offset'))
+        byterun.image_offset = Integer.from_dict(byterun_dict.get('image_offset'))
+        byterun.length = Integer.from_dict(byterun_dict.get('length'))
+        byterun.hashes = HashList.from_list(byterun_dict.get('hashes'))
+        byterun.byte_run_data = byterun_dict.get('byte_run_data')
+
+        return byterun
+
+
+class ByteRuns(cybox.EntityList):
+    _binding_class = common_binding.ByteRunsType
+    _contained_type = ByteRun
+
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Byte_Run(list_)
+
+    @staticmethod
+    def _get_list(binding_obj):
+        return binding_obj.get_Byte_Run()
