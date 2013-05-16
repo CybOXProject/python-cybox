9b1e5f50142dafc6d1117eb3701064e9c7616f45
Ivan Kirillov <ikirillov@mitre.org>
2013-05-16 13:51:05 -0300

    Update __init__.py

--- cybox/__init__.py
+++ cybox/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2013, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "2.0.0b1"
+__version__ = "2.0.0b2"
 
 import collections
 import json
