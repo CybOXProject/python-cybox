8168ad3133652e25c312919e69c9d8e86ac6af2f
Ivan Kirillov <ikirillov@mitre.org>
2013-05-16 09:43:41 -0400

    Updated AssociatedObject for CybOX 2.0

--- cybox/core/associated_object.py
+++ cybox/core/associated_object.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2013, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
+import cybox
 import cybox.utils as utils
-import cybox.bindings.cybox_core_1_0 as core_binding
+import cybox.bindings.cybox_core as core_binding
 from cybox.core.object import Object
+from cybox.common import VocabString
 
 class AssociatedObject(Object):
 
@@ -18,7 +20,7 @@ class AssociatedObject(Object):
         
     def to_obj(self):
         obj = super(AssociatedObject, self).to_obj(core_binding.AssociatedObjectType())
-        obj.set_association_type(self.association_type_)
+        obj.set_Association_Type(self.association_type_.to_obj())
         return obj
 
     def to_dict(self):
@@ -31,7 +33,7 @@ class AssociatedObject(Object):
         if not object_obj:
             return None
         obj = Object.from_obj(object_obj, AssociatedObject())
-        obj.association_type_ = object_obj.get_association_type()
+        obj.association_type_ = VocabString.from_obj(object_obj.get_association_type())
         return obj
 
     @staticmethod
@@ -39,21 +41,5 @@ class AssociatedObject(Object):
         if not object_dict:
             return None
         obj = Object.from_dict(object_dict, AssociatedObject())
-        obj.association_type_ = object_dict.get('association_type', None)
+        obj.association_type_ = VocabString.from_dict(object_dict.get('association_type', None))
         return obj
-
-
-    #@classmethod
-    #def object_from_dict(cls, associated_object_dict):
-    #    """Create the Associated Object Python object representation from an input dictionary"""
-    #    associated_obj = Object.object_from_dict(associated_object_dict,core_binding.AssociatedObjectType())
-    #    for key, value in associated_object_dict.items():
-    #        if key == 'association_type' and utils.test_value(value): associated_obj.set_association_type(value)
-    #    return associated_obj
-
-    #@classmethod
-    #def dict_from_object(cls, associated_obj):
-    #    """Parse and return a dictionary for an Associated Object"""
-    #    associated_object_dict = Object.dict_from_object(associated_obj)
-    #    if associated_obj.get_association_type() is not None: associated_object_dict['association_type'] = associated_obj.get_association_type()
-    #    return associated_object_dict
