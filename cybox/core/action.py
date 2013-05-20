83b8a5f3019095a0fa9f4c3bb28e4dfbe5a6da27
Ivan Kirillov <ikirillov@mitre.org>
2013-05-20 11:02:40 -0400

    Fleshed out to_dict, added ActionRelationship classes, some minor fixes

--- cybox/core/action.py
+++ cybox/core/action.py
@@ -6,6 +6,7 @@ import cybox.utils as utils
 import cybox.bindings.cybox_core as core_binding
 from cybox.common import VocabString, StructuredText, MeasureSource
 from cybox.core.associated_object import AssociatedObject
+from cybox.core.action_reference import ActionReference
 
 class Action(cybox.Entity):
     def __init__(self):
@@ -16,14 +17,13 @@ class Action(cybox.Entity):
         self.ordinal_position = None
         self.action_status = None
         self.context = None
-        self.network_protocol = None
         self.timestamp = None
         self.description = None
         self.action_aliases = []
-        self.action_arguments = []
+        self.action_arguments = ActionArguments()
         self.discovery_method = None
-        self.associated_objects = []
-        self.relationships = []
+        self.associated_objects = AssociatedObjects()
+        self.relationships = ActionRelationships()
         self.frequency = None
         
     def to_obj(self, action_obj = None):
@@ -31,41 +31,43 @@ class Action(cybox.Entity):
             action_obj = core_binding.ActionType()
         if self.id is not None: action_obj.set_id(self.id)
         if self.idref is not None: action_obj.set_idref(self.idref)
-        if self.type is not None: action_obj.set_Type(self.type.to_obj())
-        if self.name is not None: action_obj.set_Name(self.name.to_obj())
         if self.ordinal_position is not None: action_obj.set_ordinal_position(self.ordinal_position)
         if self.action_status is not None: action_obj.set_action_status(self.action_status)
         if self.context is not None: action_obj.set_context(self.context)
-        if self.network_protocol is not None: action_obj.set_network_protocol(self.network_protocol)
         if self.timestamp is not None: action_obj.set_timestamp(self.timestamp)
-        if self.description is not None: action_obj.set_description(self.description.to_obj())
+        if self.type is not None: action_obj.set_Type(self.type.to_obj())
+        if self.name is not None: action_obj.set_Name(self.name.to_obj())
+        if self.description is not None: action_obj.set_Description(self.description.to_obj())
         if len(self.action_aliases) > 0:
             action_aliases_obj = core_binding.ActionAliasesType()
             for action_alias in self.action_aliases:
                 action_aliases_obj.add_Action_Alias(action_alias)
             action_obj.set_Action_Aliases(action_aliases_obj)
-        if len(self.action_arguments) > 0:
-            action_arguments_obj = core_binding.ActionArgumentsType()
-            for action_argument in self.action_arguments:
-                action_arguments_obj.add_Action_Argument(action_argument.to_obj())
-            action_obj.set_Action_Arguments(action_arguments_obj)
+        if len(self.action_arguments) > 0: action_obj.set_Action_Arguments(self.action_arguments.to_obj())
         if self.discovery_method is not None: action_obj.set_Discovery_Method(self.discovery_method.to_obj())
-        if len(self.associated_objects) > 0: 
-            associated_objects_obj = core_binding.AssociatedObjectsType()
-            for associated_object in self.associated_objects:
-                associated_objects_obj.add_Associated_Object(associated_object.to_obj())
-            action_obj.set_Associated_Objects(associated_objects_obj)
-        if len(self.relationships) > 0:
-            relationships_obj = core_binding.RelationshipsType()
-            for relationship in self.relationships:
-                relationships_obj.add_Relationship(relationship.to_obj())
-            action_obj.set_Relationships(relationships_obj)
+        if len(self.associated_objects) > 0: action_obj.set_Associated_Objects(self.associated_objects.to_obj())
+        if len(self.relationships) > 0: action_obj.set_Relationships(self.relationships.to_obj())
         if self.frequency is not None: action_obj.set_Frequency(self.frequency.to_obj())
         
         return action_obj
 
     def to_dict(self):
         action_dict = {}
+        if self.id is not None: action_dict['id'] = self.id
+        if self.idref is not None: action_dict['idref'] = self.idref
+        if self.ordinal_position is not None: action_dict['ordinal_position'] = self.ordinal_position
+        if self.action_status is not None: action_dict['action_status'] = self.action_status
+        if self.context is not None: action_dict['context'] = self.context
+        if self.timestamp is not None: action_dict['timestamp'] = self.timestamp
+        if self.type is not None: action_dict['type'] = self.type.to_dict()
+        if self.name is not None: action_dict['name'] = self.name.to_dict()
+        if self.description is not None: action_dict['description'] = self.description.to_dict()
+        if len(self.action_aliases) > 0: action_dict['action_aliases'] = [x for x in self.action_aliases]
+        if len(self.action_arguments) > 0: action_dict['action_arguments'] = self.action_arguments.to_list()
+        if self.discovery_method is not None: action_dict['discovery_method'] = self.discovery_method.to_dict()
+        if len(self.associated_objects) > 0: action_dict['associated_objects'] = self.associated_objects.to_list()
+        if len(self.relationships) > 0: action_dict['relationships'] = self.relationships.to_list()
+        if self.frequency is not None: action_dict['frequency'] = self.frequency.to_dict()
         return action_dict
 
     @staticmethod
@@ -77,28 +79,49 @@ class Action(cybox.Entity):
         action_ = action_cls
         action_.id = action_dict.get('id')
         action_.idref = action_dict.get('idref')
-        action_.type = VocabString.from_dict(action_dict.get('type'))
-        action_.name = VocabString.from_dict(action_dict.get('name'))
         action_.ordinal_position = action_dict.get('ordinal_position')
         action_.action_status = action_dict.get('action_status')
         action_.context = action_dict.get('context')
-        action_.network_protocol = action_dict.get('network_protocol')
         action_.timestamp = action_dict.get('timestamp')
-        #action_.description = StructuredText.from_dict(action.get('description'))
+        action_.type = VocabString.from_dict(action_dict.get('type'))
+        action_.name = VocabString.from_dict(action_dict.get('name'))
+        action_.description = StructuredText.from_dict(action.get('description'))
         action_.action_aliases = action_dict.get('action_aliases', [])
-        action_.action_arguments = [ActionArgument.from_dict(x) for x in action_dict.get('action_arguments', [])]
-        #action_.discovery_method = MeasureSource.from_dict(action_dict.get('discovery_method'))
-        action_.associated_objects = [AssociatedObject.from_dict(x) for x in action_dict.get('associated_objects', [])]
-        action_.relationships = [ActionRelationship.from_dict(x) for x in action_dict.get('relationships', [])]
-        #action_.frequency = Frequency.from_dict(action_dict.get('frequency'))
+        action_.action_arguments = ActionArguments.from_list(action_dict.get('action_arguments', []))
+        action_.discovery_method = MeasureSource.from_dict(action_dict.get('discovery_method'))
+        action_.associated_objects = AssociatedObjects.from_list(action_dict.get('associated_objects', []))
+        action_.relationships = ActionRelationships.from_list(action_dict.get('relationships', []))
+        #action_.frequency = Frequency.from_dict(action_dict.get('frequency')) #TODO: add support
         return action_
 
     @staticmethod
-    def from_obj(action_obj):
-        pass
+    def from_obj(action_obj, action_cls = None):
+        if not action_obj:
+            return None
+        if action_cls == None:
+            action_cls = Action()
+        action_ = action_cls
+        action_.id = action_obj.get_id()
+        action_.idref = action_obj.get_idref()
+        action_.ordinal_position = action_obj.get_ordinal_position()
+        action_.action_status = action_obj.get_action_status()
+        action_.context = action_obj.get_context()
+        action_.timestamp = action_obj.get_timestamp()
+        action_.type = VocabString.from_obj(action_obj.get_Type())
+        action_.name = VocabString.from_obj(action_obj.get_Name())
+        action_.description = StructuredText.from_obj(action_obj.get_Description())
+        action_.action_arguments = ActionArguments.from_obj(action_obj.get_Action_Arguments())
+        action_.discovery_method = MeasureSource.from_obj(action_obj.get_Discovery_Method())
+        action_.associated_objects = AssociatedObjects.from_obj(action_obj.get_Associated_Objects())
+        action_.relationships = ActionRelationships.from_obj(action_obj.get_Relationships())
+        #action_.frequency = Frequency.from_dict(action_dict.get('frequency')) #TODO: add support
+        if action_obj.get_Action_Alises() is not None :
+            action_.action_aliases = action_obj.get_Action_Aliases().get_Action_Alias()
+        return action_
 
 class ActionArgument(cybox.Entity):
     def __init__(self):
+        super(ActionArgument, self).__init__()
         self.defined_argument_name = None
         self.undefined_argument_name = None
         self.argument_value = None
@@ -127,8 +150,24 @@ class ActionArgument(cybox.Entity):
     def from_obj(action_argument_obj):
         pass
 
+class ActionArguments(cybox.EntityList):
+    _contained_type = ActionArgument
+    _binding_class = core_binding.ActionArgumentsType
+
+    def __init__(self):
+        super(ActionArguments, self).__init__()
+
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Action_Argument(list_)
+
+    @staticmethod
+    def _get_list(binding_obj):
+        return binding_obj.get_Action_Argument()
+
 class ActionRelationship(cybox.Entity):
     def __init__(self):
+        super(ActionRelationship, self).__init__()
         self.type = None
         self.action_references = []
 
@@ -154,3 +193,74 @@ class ActionRelationship(cybox.Entity):
     @staticmethod
     def from_obj(action_relationship_obj):
         pass
+
+class AssociatedObjects(cybox.EntityList):
+    _contained_type = AssociatedObject
+    _binding_class = core_binding.AssociatedObjectsType
+
+    def __init__(self):
+        super(AssociatedObjects, self).__init__()
+
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Associated_Object(list_)
+
+    @staticmethod
+    def _get_list(binding_obj):
+        return binding_obj.get_Associated_Object()
+
+class ActionRelationship(cybox.Entity):
+
+    def __init__(self):
+        super(ActionRelationship, self).__init__()
+        self.type = None
+        self.action_references = []
+
+    def to_obj(self):
+        action_relationship_obj = core_binding.ActionRelationshipType()
+        if self.type is not None : action_relationship_obj.set_Type(self.type.to_obj())
+        if len(self.action_references) > 0:
+            for action_reference in self.action_references: action_relationship_obj.add_Action_Reference(action_reference.to_obj())
+        return action_relationship_obj
+
+    def to_dict(self):
+        action_relationship_dict = {}
+        if self.type is not None : action_relationship_dict['type'] = self.type.to_dict()
+        if len(self.action_references) > 0:
+            action_reference_list = []
+            for action_reference in self.action_references: action_reference_list.append(action_reference.to_dict())
+            action_relationship_dict['action_references'] = action_reference_list
+        return action_relationship_dict
+
+    @staticmethod
+    def from_dict(action_relationship_dict):
+        if not action_relationship_dict:
+            return None
+        action_relationship_ = ActionRelationship()
+        action_relationship_.type = StructuredText.from_dict(action_relationship_dict.get('type'))
+        action_relationship_.action_references = [ActionReference.from_dict(x) for x in action_relationship_dict.get('action_references')]
+        return action_relationship_
+
+    @staticmethod
+    def from_dict(action_relationship_obj):
+        if not action_relationship_obj:
+            return None
+        action_relationship_ = ActionRelationship()
+        action_relationship_.type = StructuredText.from_obj(action_relationship_obj.get_Type())
+        action_relationship_.action_references = [ActionReference.from_obj(x) for x in action_relationship_obj.get_Action_Reference()]
+        return action_relationship_
+
+class ActionRelationships(cybox.EntityList):
+    _contained_type = ActionRelationship
+    _binding_class = core_binding.RelationshipsType
+
+    def __init__(self):
+        super(ActionRelationshipList, self).__init__()
+
+    @staticmethod
+    def _set_list(binding_obj, list_):
+        binding_obj.set_Relationship(list_)
+
+    @staticmethod
+    def _get_list(binding_obj):
+        return binding_obj.get_Relationship()
