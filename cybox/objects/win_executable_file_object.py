eb9c55ffb5d5ccf47db558e8bec591b0b8e90328
Ivan Kirillov <ikirillov@mitre.org>
2013-05-22 11:13:22 -0400

    Fixed WinExecutableObjectType instantiation in to_obj()

--- cybox/objects/win_executable_file_object.py
+++ cybox/objects/win_executable_file_object.py
@@ -25,7 +25,7 @@ class WinExecutableFile(WinFile):
         self.type = None
         
     def to_obj(self):
-        win_executable_file_obj = super(WinExecutableFile, self).to_obj(win_executable_file_binding.WindowsExecutableFileObjectType)
+        win_executable_file_obj = super(WinExecutableFile, self).to_obj(win_executable_file_binding.WindowsExecutableFileObjectType())
         win_executable_file_obj.set_xsi_type(self._XSI_NS + ':' + self._XSI_TYPE)
 
         if self.build_information is not None : win_executable_file_obj.set_Build_Information(self.build_information.to_obj())
