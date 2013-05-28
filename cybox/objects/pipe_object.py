c7d4260bb90d48de8a9accce74dd585efa2bdcbe
Ivan Kirillov <ikirillov@mitre.org>
2013-05-28 09:08:30 -0400

    Made Pipe object subclass from ObjectProperties

--- cybox/objects/pipe_object.py
+++ cybox/objects/pipe_object.py
@@ -5,7 +5,7 @@ import cybox
 import cybox.bindings.pipe_object as pipe_binding
 from cybox.common import ObjectProperties, String
 
-class Pipe(cybox.Entity):
+class Pipe(ObjectProperties):
     _XSI_NS = "PipeObj"
     _XSI_TYPE = "PipeObjectType"
 
