122318205606a3debd3c24832f95f0775bb259f1
Greg Back <gback@mitre.org>
2013-05-30 23:09:58 -0400

    Fix port test by adding super() call in __init__

--- cybox/objects/port_object.py
+++ cybox/objects/port_object.py
@@ -5,11 +5,13 @@ import cybox.utils as utils
 import cybox.bindings.port_object as port_binding
 from cybox.common import ObjectProperties, String, PositiveInteger
 
+
 class Port(ObjectProperties):
     _XSI_NS = "PortObj"
     _XSI_TYPE = "PortObjectType"
 
     def __init__(self):
+        super(Port, self).__init__()
         self.port_value = None
         self.layer4_protocol = None
 
@@ -17,8 +19,10 @@ class Port(ObjectProperties):
         port_obj = port_binding.PortObjectType()
         super(Port, self).to_obj(port_obj)
 
-        if self.port_value is not None: port_obj.set_Port_Value(self.port_value.to_obj())
-        if self.layer4_protocol is not None: port_obj.set_Layer4_Protocol(self.layer4_protocol.to_obj())
+        if self.port_value is not None:
+            port_obj.set_Port_Value(self.port_value.to_obj())
+        if self.layer4_protocol is not None:
+            port_obj.set_Layer4_Protocol(self.layer4_protocol.to_obj())
 
         return port_obj
 
@@ -26,31 +30,31 @@ class Port(ObjectProperties):
         port_dict = {}
         super(Port, self).to_dict(port_dict)
 
-        if self.port_value is not None: port_dict['port_value'] = self.port_value.to_dict()
-        if self.layer4_protocol is not None: port_doct['layer4_protocol'] = self.layer4_protocol.to_dict()
+        if self.port_value is not None:
+            port_dict['port_value'] = self.port_value.to_dict()
+        if self.layer4_protocol is not None:
+            port_dict['layer4_protocol'] = self.layer4_protocol.to_dict()
 
         return port_dict
-    
+
     @staticmethod
     def from_dict(port_dict):
         if not port_dict:
             return None
 
-        port_ = Port()
-        port_.port_value = PositiveInteger.from_dict(port_dict.get('port_value'))
-        port_.layer4_protocol = String.from_dict(port_dict.get('layer4_protocol'))
+        port = Port()
+        port.port_value = PositiveInteger.from_dict(port_dict.get('port_value'))
+        port.layer4_protocol = String.from_dict(port_dict.get('layer4_protocol'))
 
-        return port_
+        return port
 
     @staticmethod
     def from_obj(port_obj):
         if not port_obj:
             return None
 
-        port_ = Port()
-        port_.port_value = PositiveInteger.from_obj(port_obj.get_Port_Value())
-        port_.layer4_protocol = String.from_obj(port_obj.get_Layer4_Protocol())
+        port = Port()
+        port.port_value = PositiveInteger.from_obj(port_obj.get_Port_Value())
+        port.layer4_protocol = String.from_obj(port_obj.get_Layer4_Protocol())
 
-        return port_
-
-
+        return port
