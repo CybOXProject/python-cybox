98d4969ed6d4eb863bc9d62642d371a267be7f65
Ivan Kirillov <ikirillov@mitre.org>
2013-06-06 10:46:32 -0400

    Updated Extracted String List to use new EntityList methodology and renamed class to ExtractedStrings to line up with the schema type

--- cybox/common/extracted_string_list.py
+++ cybox/common/extracted_string_list.py
@@ -5,42 +5,17 @@ import cybox
 import cybox.bindings.cybox_common as common_types_binding
 from cybox.common.extracted_string import ExtractedString
 
-class ExtractedStringList(cybox.EntityList):
+class ExtractedStrings(cybox.EntityList):
     _contained_type = ExtractedString
+    _binding_class = common_binding.ExtractedStringsType
 
     def __init__(self):
-        super(ExtractedStringList, self).__init__()
-
-    def _fix_value(self, value):
-        # If the user tries to put a string into a list, convert it to an ExtractedString.
-        if isinstance(value, basestring):
-            return ExtractedString(value)
-
-    def to_obj(self):
-        ex_string_list_obj = common_types_binding.ExtractedStringsType()
-        for extracted_string_ in self:
-            ex_string_list_obj.add_String(extracted_string_.to_obj())
-        return ex_string_list_obj
-
-    def to_list(self):
-        return [ex_string.to_dict() for ex_string in self]
+        super(ExtractedStrings, self).__init__()
 
     @staticmethod
-    def from_list(extracted_string_list):
-        if not extracted_string_list:
-            return None
-
-        extracted_string_list_ = ExtractedStringList()
-        for extracted_string_dict in extracted_string_list:
-            extracted_string_list.append(ExtractedString.from_dict(extracted_string_dict))
-        return extracted_string_list_
+    def _set_list(binding_obj, list_):
+        binding_obj.set_String(list_)
 
     @staticmethod
-    def from_obj(extracted_string_list_obj):
-        if not extracted_string_list_obj:
-            return None
-
-        extracted_string_list_ = ExtractedStringList()
-        for extracted_string_obj in extracted_string_list_obj.get_String():
-            extracted_string_list_.append(ExtractedString.from_obj(extracted_string_obj))
-        return extracted_string_list_
+    def _get_list(binding_obj):
+        return binding_obj.get_String()
