3e0bd3631b36f999e1ff19362faff6319ab2e86f
Ivan Kirillov <ikirillov@mitre.org>
2013-07-30 13:02:49 -0400

    Added TypedField implementation

--- cybox/objects/mutex_object.py
+++ cybox/objects/mutex_object.py
@@ -6,64 +6,12 @@ import cybox.utils as utils
 import cybox.bindings.mutex_object as mutex_binding
 from cybox.common import ObjectProperties, String
 
-
 class Mutex(ObjectProperties):
+    _binding = mutex_binding
+    _binding_class = mutex_binding.MutexObjectType
     _namespace = "http://cybox.mitre.org/objects#MutexObject-2"
     _XSI_NS = "MutexObj"
     _XSI_TYPE = "MutexObjectType"
 
-    def __init__(self):
-        super(Mutex, self).__init__()
-        self.named = None
-        self.name = None
-
-    def to_obj(self, mutex_obj=None):
-        if mutex_obj == None:
-            mutex_obj = mutex_binding.MutexObjectType()
-        super(Mutex, self).to_obj(mutex_obj)
-
-        if self.named is not None:
-            mutex_obj.set_named(self.named)
-        if self.name is not None:
-            mutex_obj.set_Name(self.name.to_obj())
-
-        return mutex_obj
-
-    def to_dict(self):
-        mutex_dict = {}
-        super(Mutex, self).to_dict(mutex_dict)
-
-        if self.named is not None:
-            mutex_dict['named'] = self.named
-        if self.name is not None:
-            mutex_dict['name'] = self.name.to_dict()
-
-        return mutex_dict
-
-    @staticmethod
-    def from_dict(mutex_dict, mutex_cls = None):
-        if not mutex_dict:
-            return None
-        if mutex_cls == None:
-            mutex_ = Mutex()
-        else:
-            mutex_ = mutex_cls
-
-        mutex_.named = mutex_dict.get('named')
-        mutex_.name = String.from_dict(mutex_dict.get('name'))
-
-        return mutex_
-
-    @staticmethod
-    def from_obj(mutex_obj, mutex_cls = None):
-        if not mutex_obj:
-            return None
-        if mutex_cls == None:
-            mutex_ = Mutex()
-        else:
-            mutex_ = mutex_cls
-
-        mutex_.named = mutex_obj.get_named()
-        mutex_.name = String.from_obj(mutex_obj.get_Name())
-
-        return mutex_
+    named = cybox.TypedField("named")
+    name = cybox.TypedField("Name", String)
--- cybox/objects/win_mutex_object.py
+++ cybox/objects/win_mutex_object.py
@@ -9,50 +9,12 @@ from cybox.common import String
 
 
 class WinMutex(Mutex):
+    _binding = win_mutex_binding
+    _binding_class = win_mutex_binding.WindowsMutexObjectType
     _namespace = "http://cybox.mitre.org/objects#WinMutexObject-2"
     _XSI_NS = "WinMutexObj"
     _XSI_TYPE = "WindowsMutexObjectType"
 
-    def __init__(self):
-        super(WinMutex, self).__init__()
-        self.handle = None
-        self.security_attributes = None
+    handle = cybox.TypedField("Handle", WinHandle)
+    security_attributes = cybox.TypedField("Security_Attributes", String)
 
-    def to_obj(self):
-        win_mutex_obj = super(WinMutex, self).to_obj(win_mutex_binding.WindowsMutexObjectType())
-
-        if self.handle is not None: win_mutex_obj.set_Handle(self.handle.to_obj())
-        if self.security_attributes is not None: win_mutex_obj.set_Security_Attributes(self.security_attributes.to_obj())
-
-        return win_mutex_obj
-
-    def to_dict(self):
-        win_mutex_dict = super(WinMutex, self).to_dict()
-
-        if self.handle is not None: win_mutex_dict['handle'] = self.handle.to_dict()
-        if self.security_attributes is not None: win_mutex_dict['security_attributes'] = self.security_attributes.to_dict()
-        win_mutex_dict['xsi:type'] = self._XSI_TYPE
-
-        return win_mutex_dict
-        
-    @staticmethod
-    def from_dict(win_mutex_dict):
-        if not win_mutex_dict:
-            return None
-
-        win_mutex_ = Mutex.from_dict(win_mutex_dict, WinMutex())
-        win_mutex_.handle = WinHandle.from_dict(win_mutex_dict.get('handle'))
-        win_mutex_.security_attributes = String.from_dict(win_mutex_dict.get('security_attributes'))
-
-        return win_mutex_
-
-    @staticmethod
-    def from_obj(win_mutex_obj):
-        if not win_mutex_obj:
-            return None
-
-        win_mutex_ = Mutex.from_obj(win_mutex_obj, WinMutex())
-        win_mutex_.handle = WinHandle.from_obj(win_mutex_obj.get_Handle())
-        win_mutex_.security_attributes = String.from_dict(win_mutex_obj.get_Security_Attributes())
-
-        return win_mutex_
