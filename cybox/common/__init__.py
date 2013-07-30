750fde9c9769a7e0fb9f3a74747b287420a2b01d
Greg Back <gback@mitre.org>
2013-07-30 12:26:57 -0400

    Use relative imports in cybox/common/__init__.py

--- cybox/common/__init__.py
+++ cybox/common/__init__.py
@@ -1,16 +1,18 @@
 # Copyright (c) 2013, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-from properties import *
-from vocabs import VocabString
-from object_properties import ObjectProperties
-from structured_text import StructuredText
-from hashes import Hash, HashList, HashName
-from tools import ToolInformation, ToolInformationList
+from __future__ import absolute_import
 
-from daterange import DateRange
-from contributor import Contributor
-from personnel import Personnel
-from time import Time
-from byterun import ByteRun, ByteRuns
-from measuresource import MeasureSource
+from .properties import *
+from .vocabs import VocabString
+from .object_properties import ObjectProperties
+from .structured_text import StructuredText
+from .hashes import Hash, HashList, HashName
+from .tools import ToolInformation, ToolInformationList
+
+from .daterange import DateRange
+from .contributor import Contributor
+from .personnel import Personnel
+from .time import Time
+from .byterun import ByteRun, ByteRuns
+from .measuresource import MeasureSource
