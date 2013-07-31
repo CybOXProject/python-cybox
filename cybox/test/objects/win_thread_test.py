d054f6400b41555ebaa7e52cd39a2d659e3e6745
Greg Back <gback@mitre.org>
2013-07-31 14:25:57 -0400

    Convert WinThread object to simpler representation.

---
+++ cybox/test/objects/win_thread_test.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2013, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import unittest
+
+from cybox.objects.win_thread_object import WinThread
+import cybox.test
+from cybox.test.objects import ObjectTestCase
+
+
+class TestWinThread(unittest.TestCase, ObjectTestCase):
+    object_type = "WindowsThreadObjectType"
+    klass = WinThread
+
+    def test_round_trip(self):
+        thread_dict = {
+                        'thread_id': 1234,
+                        'handle': {
+                                'name': u"Thread Handle",
+                                'type': u"Thread",
+                                'xsi:type': "WindowsHandleObjectType",
+                            },
+                        'running_status': u"Ready",
+                        'context': u"My Context",
+                        'priority': 15,
+                        'creation_flags': u"FFF0",
+                        'creation_time': "2013-07-31T14:08:10+05:00",
+                        'start_address': u"00400000",
+                        'parameter_address': u"01234567",
+                        'security_attributes': u"Some Attributes",
+                        'stack_size': 0x1000,
+                        'xsi:type': 'WindowsThreadObjectType'
+                      }
+        thread_dict2 = cybox.test.round_trip_dict(WinThread, thread_dict)
+        self.maxDiff = None
+        self.assertEqual(thread_dict, thread_dict2)
+
+
+if __name__ == "__main__":
+    unittest.main()
