1eefe448093d83b6ba48045d71769753aaff359d
Ivan Kirillov <ikirillov@mitre.org>
2013-08-16 07:58:34 -0400

    Fixed typo in 'name' property. Closes #49

--- cybox/objects/process_object.py
+++ cybox/objects/process_object.py
@@ -41,7 +41,7 @@ class ImageInfo(cybox.Entity):
     _binding_class = process_binding.ImageInfoType
     _namespace = "http://cybox.mitre.org/objects#ProcessObject-2"
 
-    file_name = cybox.TypedField("Name", String)
+    name = cybox.TypedField("Name", String)
     command_line = cybox.TypedField("Command_Line", String)
     current_directory = cybox.TypedField("Current_Directory", String)
     path = cybox.TypedField("Path", String)
