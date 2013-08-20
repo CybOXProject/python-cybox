8547be70e9cd18c8a736710e780a2cc470909632
Greg Back <gback@mitre.org>
2013-08-20 16:35:06 -0400

    Clean up unneeded code now that NetworkPacket is the new style.

--- cybox/utils/__init__.py
+++ cybox/utils/__init__.py
@@ -58,28 +58,3 @@ def unwrap_cdata(value):
         return value[9:-3]
     else:
         return value
-
-
-def test_value(value):
-    """
-    Test if an input string value is not None and has a length grater than 0 or
-    if a dictionary contains a "value" key whose value is not None and has
-    a length greater than 0.
-
-    We explicitly want to return True even if the value is False or 0, since
-    some parts of the standards are boolean or allow a 0 value, and we want to
-    distinguish the case where the "value" key is omitted entirely.
-    """
-    if isinstance(value, dict):
-        v = value.get('value', None)
-    elif isinstance(value, str):
-        v = value
-    elif isinstance(value, unicode):
-        v = value
-    elif isinstance(value, int):
-        v = value
-    elif isinstance(value, float):
-        v = value
-    else:
-        v = None
-    return (v is not None) and (len(str(v)) > 0)
