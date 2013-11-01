eb6d83fd905036f227a045f233783262809c80d5
Andrew Sillers <apsillers@gmail.REMOVETHISSECTION.com>
2013-11-01 12:06:03 -0400

    Adds more properties to Win Driver object

--- cybox/objects/win_driver_object.py
+++ cybox/objects/win_driver_object.py
@@ -18,6 +18,60 @@ class WinDriver(ObjectProperties):
     driver_name = TypedField("Driver_Name", String)
     driver_object_address = TypedField("Driver_Object_Address", HexBinary)
     driver_start_io = TypedField("Driver_Start_IO", HexBinary)
+    driver_unload = TypedField("Driver_Unload", HexBinary)
+    
+    irp_mj_cleanup = TypedField("IRP_MJ_CLEANUP", UnsignedLong)
+    irp_mj_close = TypedField("IRP_MJ_CLOSE", UnsignedLong)
+    irp_mj_create = TypedField("IRP_MJ_CREATE", UnsignedLong)
+    irp_mj_create_mailslot = TypedField("IRP_MJ_CREATE_MAILSLOT", UnsignedLong)
+    irp_mj_create_named_pipe = TypedField("IRP_MJ_CREATE_NAMED_PIPE", UnsignedLong)
+    irp_mj_device_change = TypedField("IRP_MJ_DEVICE_CHANGE", UnsignedLong)
+    irp_mj_device_control = TypedField("IRP_MJ_DEVICE_CONTROL", UnsignedLong)
+    irp_mj_directory_control = TypedField("IRP_MJ_DIRECTORY_CONTROL", UnsignedLong)
+    irp_mj_file_system_control = TypedField("IRP_MJ_FILE_SYSTEM_CONTROL", UnsignedLong)
+    irp_mj_flush_buffers = TypedField("IRP_MJ_FLUSH_BUFFERS", UnsignedLong)
+    irp_mj_internal_device_control = TypedField("IRP_MJ_INTERNAL_DEVICE_CONTROL", UnsignedLong)
+    irp_mj_lock_control = TypedField("IRP_MJ_LOCK_CONTROL", UnsignedLong)
+    irp_mj_pnp = TypedField("IRP_MJ_PNP", UnsignedLong)
+    irp_mj_power = TypedField("IRP_MJ_POWER", UnsignedLong)
+    irp_mj_query_ea = TypedField("IRP_MJ_QUERY_EA", UnsignedLong)
+    irp_mj_query_information = TypedField("IRP_MJ_QUERY_INFORMATION", UnsignedLong)
+    irp_mj_query_quota = TypedField("IRP_MJ_QUERY_QUOTA", UnsignedLong)
+    irp_mj_query_security = TypedField("IRP_MJ_QUERY_SECURITY", UnsignedLong)
+    irp_mj_query_volume_information = TypedField("IRP_MJ_QUERY_VOLUME_INFORMATION", UnsignedLong)
+    irp_mj_read = TypedField("IRP_MJ_READ", UnsignedLong)
+    irp_mj_set_ea = TypedField("IRP_MJ_SET_EA", UnsignedLong)
+    irp_mj_set_information = TypedField("IRP_MJ_SET_INFORMATION", UnsignedLong)
+    irp_mj_set_quota = TypedField("IRP_MJ_SET_QUOTA", UnsignedLong)
+    irp_mj_set_security = TypedField("IRP_MJ_SET_SECURITY", UnsignedLong)
+    irp_mj_set_volume_information = TypedField("IRP_MJ_SET_VOLUME_INFORMATION", UnsignedLong)
+    irp_mj_shutdown = TypedField("IRP_MJ_SHUTDOWN", UnsignedLong)
+    irp_mj_system_control = TypedField("IRP_MJ_SYSTEM_CONTROL", UnsignedLong)
     
     def __init__(self):
         super(WinDriver, self).__init__()
+    
+class DeviceObjectStruct(cybox.Entity):
+    _binding = win_driver_binding
+    _binding_class = win_driver_binding.DeviceObjectStructType
+    _namespace = "http://cybox.mitre.org/objects#WinDriverObject-2"
+    _XSI_NS = "WinDriverObj"
+    _XSI_TYPE = "DeviceObjectStructType"
+    
+    attached_device_name = TypedField("Attached_Device_Name", String)
+    attached_device_object = TypedField("Attached_Device_Object", UnsignedLong)
+    attached_to_device_name = TypedField("Attached_To_Device_Name", String)
+    attached_to_device_object = TypedField("Attached_To_Device_Object", UnsignedLong)
+    attached_to_driver_name = TypedField("Attached_To_Driver_Name", String)
+    attached_to_driver_object = TypedField("Attached_To_Driver_Object", UnsignedLong)
+    device_name = TypedField("Device_Name", String)
+    device_object = TypedField("Device_Object", UnsignedLong)
+
+class DeviceObjectList(cybox.EntityList):
+    _binding = win_driver_binding
+    _binding_class = win_driver_binding.DeviceObjectListType
+    _namespace = "http://cybox.mitre.org/objects#WinDriverObject-2"
+    _XSI_NS = "WinDriverObj"
+    _XSI_TYPE = "DeviceObjectListType"
+    _contained_type = DeviceObjectStruct
+    
