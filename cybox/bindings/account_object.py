a7c685358f754fa7ec6c9b847c5cd0752ca0e9ac
Bryan Worrell <bworrell@mitre.org>
2014-01-14 16:50:15 -0500

    Updated Account Object binding to align with CybOX 2.1

--- cybox/bindings/account_object.py
+++ cybox/bindings/account_object.py
@@ -520,20 +520,190 @@ def _cast(typ, value):
 # Data representation classes.
 #
 
+class AuthenticationType(GeneratedsSuper):
+    """The AuthenticationType type specifies authentication information for
+    an account."""
+    subclass = None
+    superclass = None
+    def __init__(self, Authentication_Type=None, Authentication_Data=None, Authentication_Token_Protection_Mechanism=None, Structured_Authentication_Mechanism=None):
+        self.Authentication_Type = Authentication_Type
+        self.Authentication_Data = Authentication_Data
+        self.Authentication_Token_Protection_Mechanism = Authentication_Token_Protection_Mechanism
+        self.Structured_Authentication_Mechanism = Structured_Authentication_Mechanism
+    def factory(*args_, **kwargs_):
+        if AuthenticationType.subclass:
+            return AuthenticationType.subclass(*args_, **kwargs_)
+        else:
+            return AuthenticationType(*args_, **kwargs_)
+    factory = staticmethod(factory)
+    def get_Authentication_Type(self): return self.Authentication_Type
+    def set_Authentication_Type(self, Authentication_Type): self.Authentication_Type = Authentication_Type
+    def get_Authentication_Data(self): return self.Authentication_Data
+    def set_Authentication_Data(self, Authentication_Data): self.Authentication_Data = Authentication_Data
+    def validate_StringObjectPropertyType(self, value):
+        # Validate type cybox_common.StringObjectPropertyType, a restriction on None.
+        pass
+    def get_Authentication_Token_Protection_Mechanism(self): return self.Authentication_Token_Protection_Mechanism
+    def set_Authentication_Token_Protection_Mechanism(self, Authentication_Token_Protection_Mechanism): self.Authentication_Token_Protection_Mechanism = Authentication_Token_Protection_Mechanism
+    def get_Structured_Authentication_Mechanism(self): return self.Structured_Authentication_Mechanism
+    def set_Structured_Authentication_Mechanism(self, Structured_Authentication_Mechanism): self.Structured_Authentication_Mechanism = Structured_Authentication_Mechanism
+    def hasContent_(self):
+        if (
+            self.Authentication_Type is not None or
+            self.Authentication_Data is not None or
+            self.Authentication_Token_Protection_Mechanism is not None or
+            self.Structured_Authentication_Mechanism is not None
+            ):
+            return True
+        else:
+            return False
+    def export(self, outfile, level, namespace_='AccountObj:', name_='AuthenticationType', namespacedef_='', pretty_print=True):
+        if pretty_print:
+            eol_ = '\n'
+        else:
+            eol_ = ''
+        showIndent(outfile, level, pretty_print)
+        outfile.write('<%s%s%s' % (namespace_, name_, namespacedef_ and ' ' + namespacedef_ or '', ))
+        already_processed = set()
+        self.exportAttributes(outfile, level, already_processed, namespace_, name_='AuthenticationType')
+        if self.hasContent_():
+            outfile.write('>%s' % (eol_, ))
+            self.exportChildren(outfile, level + 1, namespace_, name_, pretty_print=pretty_print)
+            showIndent(outfile, level, pretty_print)
+            outfile.write('</%s%s>%s' % (namespace_, name_, eol_))
+        else:
+            outfile.write('/>%s' % (eol_, ))
+    def exportAttributes(self, outfile, level, already_processed, namespace_='AccountObj:', name_='AuthenticationType'):
+        pass
+    def exportChildren(self, outfile, level, namespace_='AccountObj:', name_='AuthenticationType', fromsubclass_=False, pretty_print=True):
+        if pretty_print:
+            eol_ = '\n'
+        else:
+            eol_ = ''
+        if self.Authentication_Type is not None:
+            self.Authentication_Type.export(outfile, level, 'AccountObj:', name_='Authentication_Type', pretty_print=pretty_print)
+        if self.Authentication_Data is not None:
+            self.Authentication_Data.export(outfile, level, 'AccountObj:', name_='Authentication_Data', pretty_print=pretty_print)
+        if self.Authentication_Token_Protection_Mechanism is not None:
+            self.Authentication_Token_Protection_Mechanism.export(outfile, level, 'AccountObj:', name_='Authentication_Token_Protection_Mechanism', pretty_print=pretty_print)
+        if self.Structured_Authentication_Mechanism is not None:
+            self.Structured_Authentication_Mechanism.export(outfile, level, 'AccountObj:', name_='Structured_Authentication_Mechanism', pretty_print=pretty_print)
+    def build(self, node):
+        already_processed = set()
+        self.buildAttributes(node, node.attrib, already_processed)
+        for child in node:
+            nodeName_ = Tag_pattern_.match(child.tag).groups()[-1]
+            self.buildChildren(child, node, nodeName_)
+    def buildAttributes(self, node, attrs, already_processed):
+        pass
+    def buildChildren(self, child_, node, nodeName_, fromsubclass_=False):
+        if nodeName_ == 'Authentication_Type':
+            obj_ = cybox_common.ControlledVocabularyStringType.factory()
+            obj_.build(child_)
+            self.set_Authentication_Type(obj_)
+        elif nodeName_ == 'Authentication_Data':
+            obj_ = cybox_common.StringObjectPropertyType.factory()
+            obj_.build(child_)
+            self.set_Authentication_Data(obj_)
+        elif nodeName_ == 'Authentication_Token_Protection_Mechanism':
+            obj_ = cybox_common.ControlledVocabularyStringType.factory()
+            obj_.build(child_)
+            self.set_Authentication_Token_Protection_Mechanism(obj_)
+        elif nodeName_ == 'Structured_Authentication_Mechanism':
+            obj_ = StructuredAuthenticationMechanismType.factory()
+            obj_.build(child_)
+            self.set_Structured_Authentication_Mechanism(obj_)
+# end class AuthenticationType
+
+class StructuredAuthenticationMechanismType(GeneratedsSuper):
+    """Characterizes the description of an authentication mechanism, such
+    as biometrics-based authentication.In addition to capturing
+    basic information, this type is intended to be extended to
+    enable the structured description of an authentication mechanism
+    using the XML Schema extension feature. No extension is provided
+    by CybOX to support this, however those wishing to represent
+    structured authentication mechanism information may develop such
+    an extension."""
+    subclass = None
+    superclass = None
+    def __init__(self, Description=None):
+        self.Description = Description
+    def factory(*args_, **kwargs_):
+        if StructuredAuthenticationMechanismType.subclass:
+            return StructuredAuthenticationMechanismType.subclass(*args_, **kwargs_)
+        else:
+            return StructuredAuthenticationMechanismType(*args_, **kwargs_)
+    factory = staticmethod(factory)
+    def get_Description(self): return self.Description
+    def set_Description(self, Description): self.Description = Description
+    def hasContent_(self):
+        if (
+            self.Description is not None
+            ):
+            return True
+        else:
+            return False
+    def export(self, outfile, level, namespace_='AccountObj:', name_='StructuredAuthenticationMechanismType', namespacedef_='', pretty_print=True):
+        if pretty_print:
+            eol_ = '\n'
+        else:
+            eol_ = ''
+        showIndent(outfile, level, pretty_print)
+        outfile.write('<%s%s%s' % (namespace_, name_, namespacedef_ and ' ' + namespacedef_ or '', ))
+        already_processed = set()
+        self.exportAttributes(outfile, level, already_processed, namespace_, name_='StructuredAuthenticationMechanismType')
+        if self.hasContent_():
+            outfile.write('>%s' % (eol_, ))
+            self.exportChildren(outfile, level + 1, namespace_, name_, pretty_print=pretty_print)
+            showIndent(outfile, level, pretty_print)
+            outfile.write('</%s%s>%s' % (namespace_, name_, eol_))
+        else:
+            outfile.write('/>%s' % (eol_, ))
+    def exportAttributes(self, outfile, level, already_processed, namespace_='AccountObj:', name_='StructuredAuthenticationMechanismType'):
+        pass
+    def exportChildren(self, outfile, level, namespace_='AccountObj:', name_='StructuredAuthenticationMechanismType', fromsubclass_=False, pretty_print=True):
+        if pretty_print:
+            eol_ = '\n'
+        else:
+            eol_ = ''
+        if self.Description is not None:
+            self.Description.export(outfile, level, 'AccountObj:', name_='Description', pretty_print=pretty_print)
+    def build(self, node):
+        already_processed = set()
+        self.buildAttributes(node, node.attrib, already_processed)
+        for child in node:
+            nodeName_ = Tag_pattern_.match(child.tag).groups()[-1]
+            self.buildChildren(child, node, nodeName_)
+    def buildAttributes(self, node, attrs, already_processed):
+        pass
+    def buildChildren(self, child_, node, nodeName_, fromsubclass_=False):
+        if nodeName_ == 'Description':
+            obj_ = cybox_common.StringObjectPropertyType.factory()
+            obj_.build(child_)
+            self.set_Description(obj_)
+# end class StructuredAuthenticationMechanismType
+
+
 class AccountObjectType(cybox_common.ObjectPropertiesType):
     """The AccountObjectType type is intended to characterize generic
     accounts.The disabled field specifies whether or not the account
     is disabled.The locked_out field specifies whether or not the
     account is locked out."""
-    
     subclass = None
     superclass = cybox_common.ObjectPropertiesType
-    def __init__(self, object_reference=None, Custom_Properties=None, xsi_type=None, disabled=None, locked_out=None, Description=None, Domain=None):
-        super(AccountObjectType, self).__init__(object_reference, Custom_Properties, xsi_type )
+    def __init__(self, object_reference=None, Custom_Properties=None, disabled=None, locked_out=None, Description=None, Domain=None, Authentication=None, Creation_Date=None, Modified_Date=None, Last_Accessed_Time=None):
+        super(AccountObjectType, self).__init__(object_reference, Custom_Properties, )
         self.disabled = _cast(bool, disabled)
         self.locked_out = _cast(bool, locked_out)
         self.Description = Description
         self.Domain = Domain
+        if Authentication is None:
+            self.Authentication = []
+        else:
+            self.Authentication = Authentication
+        self.Creation_Date = Creation_Date
+        self.Modified_Date = Modified_Date
+        self.Last_Accessed_Time = Last_Accessed_Time
     def factory(*args_, **kwargs_):
         if AccountObjectType.subclass:
             return AccountObjectType.subclass(*args_, **kwargs_)
@@ -547,6 +717,16 @@ class AccountObjectType(cybox_common.ObjectPropertiesType):
         pass
     def get_Domain(self): return self.Domain
     def set_Domain(self, Domain): self.Domain = Domain
+    def get_Authentication(self): return self.Authentication
+    def set_Authentication(self, Authentication): self.Authentication = Authentication
+    def add_Authentication(self, value): self.Authentication.append(value)
+    def insert_Authentication(self, index, value): self.Authentication[index] = value
+    def get_Creation_Date(self): return self.Creation_Date
+    def set_Creation_Date(self, Creation_Date): self.Creation_Date = Creation_Date
+    def get_Modified_Date(self): return self.Modified_Date
+    def set_Modified_Date(self, Modified_Date): self.Modified_Date = Modified_Date
+    def get_Last_Accessed_Time(self): return self.Last_Accessed_Time
+    def set_Last_Accessed_Time(self, Last_Accessed_Time): self.Last_Accessed_Time = Last_Accessed_Time
     def get_disabled(self): return self.disabled
     def set_disabled(self, disabled): self.disabled = disabled
     def get_locked_out(self): return self.locked_out
@@ -555,6 +735,10 @@ class AccountObjectType(cybox_common.ObjectPropertiesType):
         if (
             self.Description is not None or
             self.Domain is not None or
+            self.Authentication or
+            self.Creation_Date is not None or
+            self.Modified_Date is not None or
+            self.Last_Accessed_Time is not None or
             super(AccountObjectType, self).hasContent_()
             ):
             return True
@@ -594,6 +778,14 @@ class AccountObjectType(cybox_common.ObjectPropertiesType):
             self.Description.export(outfile, level, 'AccountObj:', name_='Description', pretty_print=pretty_print)
         if self.Domain is not None:
             self.Domain.export(outfile, level, 'AccountObj:', name_='Domain', pretty_print=pretty_print)
+        for Authentication_ in self.Authentication:
+            Authentication_.export(outfile, level, 'AccountObj:', name_='Authentication', pretty_print=pretty_print)
+        if self.Creation_Date is not None:
+            self.Creation_Date.export(outfile, level, 'AccountObj:', name_='Creation_Date', pretty_print=pretty_print)
+        if self.Modified_Date is not None:
+            self.Modified_Date.export(outfile, level, 'AccountObj:', name_='Modified_Date', pretty_print=pretty_print)
+        if self.Last_Accessed_Time is not None:
+            self.Last_Accessed_Time.export(outfile, level, 'AccountObj:', name_='Last_Accessed_Time', pretty_print=pretty_print)
     def build(self, node):
         already_processed = set()
         self.buildAttributes(node, node.attrib, already_processed)
@@ -629,6 +821,22 @@ class AccountObjectType(cybox_common.ObjectPropertiesType):
             obj_ = cybox_common.StringObjectPropertyType.factory()
             obj_.build(child_)
             self.set_Domain(obj_)
+        elif nodeName_ == 'Authentication':
+            obj_ = AuthenticationType.factory()
+            obj_.build(child_)
+            self.Authentication.append(obj_)
+        elif nodeName_ == 'Creation_Date':
+            obj_ = cybox_common.DateTimeObjectPropertyType.factory()
+            obj_.build(child_)
+            self.set_Creation_Date(obj_)
+        elif nodeName_ == 'Modified_Date':
+            obj_ = cybox_common.DateTimeObjectPropertyType.factory()
+            obj_.build(child_)
+            self.set_Modified_Date(obj_)
+        elif nodeName_ == 'Last_Accessed_Time':
+            obj_ = cybox_common.DateTimeObjectPropertyType.factory()
+            obj_.build(child_)
+            self.set_Last_Accessed_Time(obj_)
         super(AccountObjectType, self).buildChildren(child_, node, nodeName_, True)
 # end class AccountObjectType
 
@@ -648,7 +856,6 @@ GDSClassesMapping = {
     'Code_Snippets': cybox_common.CodeSnippetsType,
     'Value': cybox_common.StringObjectPropertyType,
     'Length': cybox_common.IntegerObjectPropertyType,
-    'Reference': cybox_common.ReferenceType,
     'Encoding': cybox_common.ControlledVocabularyStringType,
     'Internationalization_Settings': cybox_common.InternationalizationSettingsType,
     'Tool_Configuration': cybox_common.ToolConfigurationType,
