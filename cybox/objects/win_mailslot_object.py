a0b3ca25f9ffab7ea722b088e53521c383cb9545
Ivan Kirillov <ikirillov@mitre.org>
2014-01-16 12:46:28 -0500

    Updated handle property to use WinHandle class

--- cybox/objects/win_mailslot_object.py
+++ cybox/objects/win_mailslot_object.py
@@ -4,7 +4,7 @@
 import cybox
 import cybox.bindings.win_mailslot_object as win_mailslot_binding
 from cybox.common import NonNegativeInteger, ObjectProperties, String
-from cybox.objects.win_handle_object import WinHandleList
+from cybox.objects.win_handle_object import WinHandle
 
 
 class WinMailslot(ObjectProperties):
@@ -14,7 +14,7 @@ class WinMailslot(ObjectProperties):
     _XSI_NS = "WinMailslotObj"
     _XSI_TYPE = "WindowsMailslotObjectType"
 
-    handle = cybox.TypedField("Handle", WinHandleList)
+    handle = cybox.TypedField("Handle", WinHandle)
     max_message_size = cybox.TypedField("Max_Message_Size", NonNegativeInteger)
     name = cybox.TypedField("Name", String)
     read_timeout = cybox.TypedField("Read_Timeout", NonNegativeInteger)
