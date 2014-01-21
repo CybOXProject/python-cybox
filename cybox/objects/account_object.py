d0bdf89f66d9947ba79783654f8d883882f4ed51
Ivan Kirillov <ikirillov@mitre.org>
2014-01-21 15:49:23 -0500

    Added new properties and classes

--- cybox/objects/account_object.py
+++ cybox/objects/account_object.py
@@ -3,8 +3,28 @@
 
 import cybox
 import cybox.bindings.account_object as account_binding
-from cybox.common import ObjectProperties, String, DateTime
+from cybox.common import ObjectProperties, String, DateTime, VocabString
 
+class StructuredAuthenticationMechanism(cybox.Entity):
+    _binding = account_binding
+    _binding_class = account_binding.StructuredAuthenticationMechanismType
+    _namespace = 'http://cybox.mitre.org/objects#AccountObject-2'
+    _XSI_NS = "AccountObj"
+    _XSI_TYPE = "AccountObjectType"
+    
+    description = cybox.TypedField("Description", String)   
+    
+class Authentication(cybox.Entity):
+    _binding = account_binding
+    _binding_class = account_binding.AuthenticationType
+    _namespace = 'http://cybox.mitre.org/objects#AccountObject-2'
+    _XSI_NS = "AccountObj"
+    _XSI_TYPE = "AccountObjectType"
+    
+    authentication_type = cybox.TypedField("Authentication_Type", VocabString)
+    authentication_data = cybox.TypedField("Authentication_Data", String)
+    authentication_token_protection_mechanism = cybox.TypedField("Authentication_Token_Protection_Mechanism", VocabString)
+    structured_authentication_mechanism = cybox.TypedField("Structured_Authentication_Mechanism", StructuredAuthenticationMechanism)
 
 class Account(ObjectProperties):
     _binding = account_binding
@@ -17,6 +37,8 @@ class Account(ObjectProperties):
     locked_out = cybox.TypedField("locked_out")
     description = cybox.TypedField("Description", String)
     domain = cybox.TypedField("Domain", String)
+    authentication = cybox.TypedField("Authentication", Authentication, multiple=True)
     creation_date = cybox.TypedField("Creation_Date", DateTime)
     modified_date = cybox.TypedField("Modified_Date", DateTime)
     last_accessed_time = cybox.TypedField("Last_Accessed_Time", DateTime)
+ 
