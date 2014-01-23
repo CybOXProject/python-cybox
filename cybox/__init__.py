dcb30ef515b3af347204143f6fb31fd47f13dd66
Greg Back <gback@mitre.org>
2014-01-23 10:13:31 -0500

    Bump version to 2.0.1.4

--- cybox/__init__.py
+++ cybox/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2013, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "2.0.1.3"
+__version__ = "2.0.1.4"
 
 import collections
 import inspect
