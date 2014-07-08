69d538080b2da821ee70d265494edde55075f6d6
Ivan Kirillov <ikirillov@mitre.org>
2014-07-08 09:00:34 -0400

    Initial commit

---
+++ cybox/utils/normalize.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import re
+from cybox.objects.file_object import File
+from cybox.objects.win_registry_key_object import WinRegistryKey
+from cybox.objects.process_object import Process
+from cybox.objects.mutex_object import Mutex
+
+# Normalization-related mappings
+
+# Windows-specific file normalization mappings
+# Replace with CSIDL values, if possible
+file_path_normalization_mapping = [{'search_string' : '%System%', 
+                                    'replacement' : 'CSIDL_SYSTEM'},
+                                    {'search_string' : '%SYSTEMROOT%', 
+                                     'replacement' : 'CSIDL_WINDOWS'},
+                                    {'search_string' : '%Windir%', 
+                                     'replacement' : 'CSIDL_WINDOWS'},
+                                    {'search_string' : '%AppData%', 
+                                     'replacement' : 'CSIDL_APPDATA'},
+                                    {'search_string' : '%CommonAppData%', 
+                                     'replacement' : 'CSIDL_COMMON_APPDATA'},
+                                    {'search_string' : '%CommonPrograms%', 
+                                     'replacement' : 'CSIDL_COMMON_PROGRAMS'},
+                                    {'search_string' : '%ProgramFiles%',  
+                                     'replacement' : 'CSIDL_PROGRAM_FILES'},
+                                    {'search_string' : '%Temp%', 
+                                     'replacement' : 'TEMP'},
+                                    {'search_string' : '%Profiles%', 
+                                     'replacement' : 'CSIDL_PROFILE'},
+                                    {'regex_string' : '[\w][:]\\\\Windows\\\\System32', 
+                                     'replacement' : 'CSIDL_SYSTEM'},
+                                    {'regex_string' : '[\w][:]\\\\Windows(?:(?!\\\\System32))', 
+                                     'replacement' : 'CSIDL_WINDOWS'},
+                                    {'regex_string' : '[\w][:]\\\\[A-Z+a-z~ ()0-9\\\\]+\\\\Application Data', 
+                                     'replacement' : 'CSIDL_APPDATA'},
+                                    {'regex_string' : '[\w][:]\\\\[A-Z+a-z~ ()0-9\\\\]+\\\\All Users\\\\Application Data', 
+                                     'replacement' : 'CSIDL_COMMON_APPDATA'},
+                                    {'regex_string' : '[\w][:]\\\\[A-Z+a-z~ ()0-9\\\\]+\\\\All Users\\\\Start Menu\\\\Programs', 
+                                     'replacement' : 'CSIDL_COMMON_PROGRAMS'},
+                                    {'regex_string' : '[\w][:]\\\\[A-Z+a-z~ ()0-9\\\\]+\\\\Temp', 
+                                     'replacement' : 'TEMP'},
+                                    {'regex_string' : '[\w][:]\\\\Users\\\\[A-Z+a-z~ ()0-9]+', 
+                                     'replacement' : 'CSIDL_PROFILE'}]
+
+# Normalization-related methods
+
+def perform_replacement(entity, mapping_list):
+    '''Perform a replacement on the value of an entity using a replacement mapping, if applicable'''
+    # Make sure the entity has a value to begin with
+    if not entity.value:
+        return
+    entity_value = entity.value
+    # Attempt the replacement
+    for mapping_dict in mapping_list:
+        # Do the direct replacement, if applicable
+        if 'search_string' in mapping_dict.keys():
+            search_string = mapping_dict['search_string']
+            replacement = mapping_dict['replacement']
+            if search_string in entity_value:
+                entity.value = entity_value.replace(search_string, replacement)
+        # Do the regex replacement, if applicable
+        if 'regex_string' in mapping_dict.keys():
+            regex_string = mapping_dict['regex_string']
+            replacement = mapping_dict['replacement']
+            if re.search(regex_string, entity_value):
+                entity.value = re.sub(regex_string, replacement, entity_value)
+
+def normalize_object_properties(object_properties):
+    '''Normalize the values of an ObjectProperties instance'''
+    # Normalize file object properties/subclasses
+    if isinstance(object_properties, File):
+        # Normalize any windows-related file paths
+        if object_properties.file_path:
+            perform_replacement(object_properties.file_path, file_path_normalization_mapping)
+    # Normalize windows registry key object properties
+    elif isinstance(object_properties, WinRegistryKey):
+        # Normalize any windows-related file paths in a registry key value
+        if object_properties.values:
+            for registry_value in object_properties.values:
+                if registry_value.data:
+                    perform_replacement(registry_value.data, file_path_normalization_mapping)
+    # Normalize process object properties/subclasses
+    elif isinstance(object_properties, Process):
+        # Normalize any windows-related file paths in the process image path
+        if object_properties.image_info and object_properties.image_info.path:
+            perform_replacement(object_properties.image_info.path, file_path_normalization_mapping)
+    # Normalize mutex object properties/subclasses
+    elif isinstance(object_properties, Mutex):
+        # Normalize some oddities in mutex-name reporting
+        if object_properties.name:
+            mutex_name = object_properties.name.value
+            if mutex_name.startswith('C:\\'):
+                object_properties.name.value = mutex_name[3:]
+            
