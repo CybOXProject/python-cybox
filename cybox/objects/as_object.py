09c3ae98903cf28bddca50c4b2ab13ab6b23054a
Greg Back <gtback@gmail.com>
2014-07-16 16:23:03 -0500

    Add AS Object. Fix #173

---
+++ cybox/objects/as_object.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import cybox
+import cybox.bindings.as_object as as_binding
+from cybox.common import NonNegativeInteger, ObjectProperties, String
+
+
+class AutonomousSystem(ObjectProperties):
+    _binding = as_binding
+    _binding_class = as_binding.ASObjectType
+    _namespace = "http://cybox.mitre.org/objects#ASObject-1"
+    _XSI_NS = "ASObj"
+    _XSI_TYPE = "ASObjectType"
+
+    number = cybox.TypedField("Number", NonNegativeInteger)
+    name = cybox.TypedField("Name", String)
+    handle = cybox.TypedField("Handle", String)
+    regional_internet_registry = cybox.TypedField("Regional_Internet_Registry", String)
+
+# Add alias for the "proper", but completely unhelpful name, "AS"
+AS = AutonomousSystem
+
---
+++ cybox/test/objects/as_test.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import unittest
+
+from cybox.objects.as_object import AutonomousSystem
+from cybox.test.objects import ObjectTestCase
+
+
+class TestAccount(ObjectTestCase, unittest.TestCase):
+    object_type = "ASObjectType"
+    klass = AutonomousSystem
+
+    _full_dict = {
+        'number': 22222,
+        'name': u"OMAHASTEAKS",
+        'handle': u"AS22222",
+        'regional_internet_registry': u"ARIN",
+        'xsi:type': object_type,
+    }
+
+
+if __name__ == "__main__":
+    unittest.main()
