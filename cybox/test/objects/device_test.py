58ed46aef909658a704f429c67bd88477a13afc0
Shayne Snow <ssnow@mitre.org>
2014-07-29 14:37:49 -0400

    Add API classes for Semaphore, DNSCache, and Device

---
+++ cybox/test/objects/device_test.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import unittest
+
+from cybox.objects.device_object import Device
+from cybox.test.objects import ObjectTestCase
+
+
+class TestAccount(ObjectTestCase, unittest.TestCase):
+    object_type = "DeviceObjectType"
+    klass = Device
+
+    _full_dict = {
+        'description': "Just a description",
+        'device_type': "GPU",
+        'manufacturer': "NVIDIA",
+        'model': "GTX 670",
+        'serial_number': "412984-124123-test",
+        'firmware_version': "v2",
+        'xsi:type': object_type,
+    }
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ cybox/test/objects/semaphore_test.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import unittest
+
+from cybox.objects.semaphore_object import Semaphore
+from cybox.test.objects import ObjectTestCase
+
+
+class TestSemaphore(ObjectTestCase, unittest.TestCase):
+    object_type = "SemaphoreObjectType"
+    klass = Semaphore
+
+    _full_dict = {
+        'named': False,
+        'current_count': 100,
+        'maximum_count': 250,
+        'name': "A Test",
+        'xsi:type': object_type
+    }
+
+
+if __name__ == "__main__":
+    unittest.main()
