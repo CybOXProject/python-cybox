7c73f83ac397853fe2508d9446c77ef79f10042e
Shayne Snow <ssnow@mitre.org>
2014-08-20 14:21:17 -0400

    Add SMS message API object and test

---
+++ cybox/objects/sms_message_object.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+
+import cybox
+import cybox.bindings.sms_message_object as sms_binding
+from cybox.common import ObjectProperties, HexBinary, String, DateTime, Integer, PositiveInteger
+
+
+class SMSMessage(ObjectProperties):
+    _binding = sms_binding
+    _binding_class = sms_binding.SMSMessageObjectType
+    _namespace = 'http://cybox.mitre.org/objects#SMSMessageObject-1'
+    _XSI_NS = 'SMSMessageObj'
+    _XSI_TYPE = "SMSMessageObjectType"
+
+    is_premium = cybox.TypedField("is_premium")
+    sender_phone_number = cybox.TypedField("Sender_Phone_Number", String)
+    recipient_phone_number = cybox.TypedField("Recipient_Phone_Number", String)
+    sent_datetime = cybox.TypedField("Sent_DateTime", DateTime)
+    body = cybox.TypedField("Body", String)
+    length = cybox.TypedField("Length", Integer)
+    size = cybox.TypedField("Size", Integer)
+    encoding = cybox.TypedField("Encoding", String)
+    bits_per_character = cybox.TypedField("Bits_Per_Character", PositiveInteger)
+    user_data_header = cybox.TypedField("User_Data_Header", HexBinary)
---
+++ cybox/test/objects/sms_message_test.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import unittest
+
+from cybox.objects.sms_message_object import SMSMessage
+
+from cybox.test import EntityTestCase, round_trip
+from cybox.test.objects import ObjectTestCase
+
+
+class TestSMSMessage(ObjectTestCase, unittest.TestCase):
+    object_type = "SMSMessageObjectType"
+    klass = SMSMessage
+
+    _full_dict = {
+        'is_premium': False,
+        'sender_phone_number': '903-5768',
+        'recipient_phone_number': '867-5309',
+        'body': "Just a body",
+        'length': 100,
+        'size': 150,
+        'encoding': "sample encoding",
+        'bits_per_character': 8,
+        'user_data_header': "abc3298edf",
+        'xsi:type': object_type,
+    }
+
+if __name__ == "__main__":
+    unittest.main()
