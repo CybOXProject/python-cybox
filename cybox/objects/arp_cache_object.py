f9587ca342512cd73d7262f32b73a9945c4f5257
Shayne Snow <ssnow@mitre.org>
2014-08-20 13:52:47 -0400

    Add ARP Cache API and test

---
+++ cybox/objects/arp_cache_object.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import cybox
+import cybox.bindings.arp_cache_object as arp_binding
+from cybox.objects.address_object import Address
+from cybox.objects.system_object import NetworkInterface
+from cybox.common import ObjectProperties, String, HexBinary, StructuredText, PlatformSpecification
+
+class ARPCacheEntry(cybox.Entity):
+    _binding = arp_binding
+    _binding_class = arp_binding.ARPCacheEntryType
+    _namespace = "http://cybox.mitre.org/objects#ARPCacheObject-1"
+    
+    ip_address = cybox.TypedField("IP_Address", Address)
+    physical_address = cybox.TypedField("Physical_Address", String)
+    type_ = cybox.TypedField("Type", String)
+    network_interface = cybox.TypedField("Network_Interface", NetworkInterface)
+
+class ARPCache(ObjectProperties):
+    _binding = arp_binding
+    _binding_class = arp_binding.ARPCacheObjectType
+    _namespace = "http://cybox.mitre.org/objects#ARPCacheObject-1"
+    _XSI_NS = "ARPCacheObj"
+    _XSI_TYPE = "ARPCacheObjectType"
+
+    arp_cache_entry = cybox.TypedField("ARP_Cache_Entry", ARPCacheEntry, multiple=True)
---
+++ cybox/test/objects/arp_cache_test.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+import unittest
+
+from cybox.objects.arp_cache_object import ARPCache
+
+from cybox.test import EntityTestCase, round_trip
+from cybox.test.objects import ObjectTestCase
+
+
+class TestARPCache(ObjectTestCase, unittest.TestCase):
+    object_type = "ARPCacheObjectType"
+    klass = ARPCache
+
+    _full_dict = {
+        'arp_cache_entry': [
+            {
+                'ip_address': {
+                    'address_value': u"100.200.100.1",
+                    'xsi:type': 'AddressObjectType'
+                }, 
+                'physical_address': u"100.200.100.1",
+                'type': u"Test",
+                'network_interface': {
+                    'adapter': u'eth0',
+                    'description': u'a test'
+                }
+            },
+            {
+                'ip_address': {
+                    'address_value': u"100.200.100.2",
+                    'xsi:type': 'AddressObjectType'
+                }, 
+                'physical_address': u"100.200.100.2",
+                'type': u"Test 2",
+                'network_interface': {
+                    'adapter': u'eth2',
+                    'description': u'a test 2'
+                }
+            }
+        ],
+        'xsi:type': object_type,
+    }
+
+if __name__ == "__main__":
+    unittest.main()
