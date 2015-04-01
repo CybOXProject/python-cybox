90a6e02ab9994dae5a3b1b4cb9c989f78e20056d
Greg Back <gback@mitre.org>
2015-04-01 16:15:36 -0400

    Normalize dev version number.

--- cybox/__init__.py
+++ cybox/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-__version__ = "2.1.0.11-dev"
+__version__ = "2.1.0.11.dev0"
 
 import collections
 import inspect
