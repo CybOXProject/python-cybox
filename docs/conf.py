6e3811a70f46fd8e2204f296cd8ecd27ddc92ed4
Greg Back <gback@mitre.org>
2015-04-03 10:35:52 -0400

    Clean up and standardize docs and conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,15 +9,17 @@ release = version
 
 extensions = [
     'sphinx.ext.autodoc',
-    'cybox.utils.autoentity',
     'sphinx.ext.doctest',
     'sphinx.ext.ifconfig',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinxcontrib.napoleon',
+    'cybox.utils.autoentity',  # TODO: move to mixbox.
 ]
 
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/', None),
+}
 
 templates_path = ['_templates']
 source_suffix = '.rst'
@@ -27,22 +29,20 @@ rst_prolog = """
 **Version**: {0}
 """.format(release)
 
-exclude_patterns = ['_build']
-pygments_style = 'sphinx'
+exclude_patterns = [
+    '_build',
+]
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
     html_theme = 'default'
 
-html_sidebars = {"**": ['localtoc.html', 'relations.html', 'sourcelink.html',
-'searchbox.html', 'links.html']}
-
 latex_elements = {}
 latex_documents = [
-  ('index', 'python-cybox.tex', u'python-cybox Documentation',
-   u'The MITRE Corporation', 'manual'),
+    ('index', 'python-cybox.tex', u'python-cybox Documentation',
+     u'The MITRE Corporation', 'manual'),
 ]
