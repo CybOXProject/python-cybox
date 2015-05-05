f677c897b60e91d8ed01a884857113dd5de438fd
Greg Back <gback@mitre.org>
2015-05-04 20:27:42 -0500

    Add mixbox to dependencies

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_version():
 with open('README.rst') as f:
     readme = f.read()
 
-install_requires = ['lxml>=2.2.3', 'python-dateutil', 'six>=1.9.0']
+install_requires = ['mixbox', 'lxml>=2.2.3', 'python-dateutil', 'six>=1.9.0']
 try:
     import importlib
 except ImportError:
