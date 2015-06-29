370e540c689c7bff649c5da7e804616ff4dddd76
Greg Back <gback@mitre.org>
2015-06-29 10:29:42 -0500

    Use nosetests to exclude CIQ extension tests from RHEL 6 tox env.

--- cybox/test/extensions/location/ciq_test.py
+++ cybox/test/extensions/location/ciq_test.py
@@ -5,21 +5,11 @@
 
 import unittest
 
-from mixbox.binding_utils import etree_
 from mixbox.vendor.six import StringIO
-
 
 class CIQAddressTests(unittest.TestCase):
 
-    def test_can_load_extension(self):
-        if etree_.LXML_VERSION < (2, 3, 0, 0):
-            # Older versions of LXML do not support `register_namespace` needed
-            # for CybOX extensions to work. This is only an issue during RHEL6
-            # tests, which also use Python 2.6, meaning that unittest.skipTest
-            # is not implemented. So we just 'return' here.
-            return
-            # self.skipTest("LXML >= 2.3 required to use extensions")
-
+    def test_can_load_ciq_extension(self):
         from cybox.bindings.extensions.location import ciq_address_3_0
         addr = ciq_address_3_0.CIQAddress3_0InstanceType()
 
