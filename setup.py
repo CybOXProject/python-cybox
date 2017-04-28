c3d1ee8fa5851a78652cdfc7469c1f8b264a73c0
Greg Back <gback@mitre.org>
2017-04-28 15:29:43 -0500

    Update tox/travis; add Python 3.6 support.

--- setup.py
+++ setup.py
@@ -34,17 +34,6 @@ try:
 except ImportError:
     install_requires.append('importlib')
 
-extras_require = {
-    'docs': [
-        'Sphinx==1.3.1',
-        'sphinx_rtd_theme==0.1.8',
-    ],
-    'test': [
-        "nose==1.3.0",
-        "tox==1.6.1"
-    ],
-}
-
 setup(
     name="cybox",
     version=get_version(),
@@ -55,12 +44,18 @@ setup(
     url="http://cybox.mitre.org",
     packages=find_packages(),
     install_requires=install_requires,
-    extras_require=extras_require,
     classifiers=[
-        "Programming Language :: Python",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
