706b640a31b08142562997b8d561ad2475b44cd4
Greg Back <gback@mitre.org>
2017-10-16 14:39:33 +0000

    Add test for #280

--- cybox/test/core/event_test.py
+++ cybox/test/core/event_test.py
@@ -5,7 +5,7 @@ import unittest
 
 from mixbox.vendor.six import u
 
-from cybox.core import Event
+from cybox.core import Event, Observable
 from cybox.test import EntityTestCase
 
 
@@ -35,6 +35,19 @@ class TestEvent(EntityTestCase, unittest.TestCase):
         e.description = "Foo"
         e.event = None
         self.assertTrue(b"Foo" in e.to_xml())
+
+    def test_observable_from_dict_with_event(self):
+        data = {
+            'event': {
+                'type': {
+                    'xsi:type': 'cyboxVocabs:EventTypeVocab-1.0.1',
+                    'value': 'DHCP',
+                }
+            }
+        }
+        obs = Observable.from_dict(data)
+        assert obs.event.type_ == 'DHCP'
+
 
 
 if __name__ == "__main__":
