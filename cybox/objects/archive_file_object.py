ac1087403b3d49da34b8cfef34b12220a312cb9a
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2020-02-21 09:50:20 -0500

    implement a few of the missing properties for: Archive, ARPCache, Device, Disk and Process

--- cybox/objects/archive_file_object.py
+++ cybox/objects/archive_file_object.py
@@ -4,8 +4,24 @@
 from mixbox import fields
 
 import cybox.bindings.archive_file_object as archive_binding
-from cybox.common import Integer, String
+from cybox.common import BaseProperty, Cipher, Integer, String
 from cybox.objects.file_object import File
+
+
+class ArchiveFileFormat(BaseProperty):
+    _binding = archive_binding
+    _binding_class = archive_binding.ArchiveFileFormatType
+    _namespace = 'http://cybox.mitre.org/objects#ArchiveFileObject-1'
+
+    TERM_7ZIP = "7-ZIP"
+    TERM_APK = "APK"
+    TERM_CAB = "CAB"
+    TERM_DMG = "DMG"
+    TERM_JAR = "JAR"
+    TERM_RAR = "RAR"
+    TERM_SIT = "SIT"
+    TERM_TGZ = "TGZ"
+    TERM_ZIP = "ZIP"
 
 
 class ArchiveFile(File):
@@ -15,10 +31,10 @@ class ArchiveFile(File):
     _XSI_NS = "ArchiveFileObj"
     _XSI_TYPE = "ArchiveFileObjectType"
 
-    archive_format = fields.TypedField("Archive_Format", String)
+    archive_format = fields.TypedField("Archive_Format", ArchiveFileFormat)
     version = fields.TypedField("Version", String)
     file_count = fields.TypedField("File_Count", Integer)
-    encryption_algorithm = fields.TypedField("Encryption_Algorithm", String) # TODO: Cipher may need its own class
+    encryption_algorithm = fields.TypedField("Encryption_Algorithm", Cipher)
     decryption_key = fields.TypedField("Decryption_Key", String)
     comment = fields.TypedField("Comment", String)
     archived_file = fields.TypedField("Archived_File", File, multiple=True)
